74ccb495c3227191c407dc0b5cc11376445be8b3
Delnegend <kiennguyen19323@gmail.com>
2025-04-18 00:09:53 +0700

    refactor(collections/Pages,Posts): rename access fn names

--- src/collections/Pages/index.ts
+++ src/collections/Pages/index.ts
@@ -1,14 +1,14 @@
 import type { CollectionConfig } from 'payload'
 
-import { authenticated } from '../../access/authenticated'
-import { authenticatedOrPublished } from '../../access/authenticatedOrPublished'
+import { slugField } from '@/fields/slug'
+import { hero } from '@/heros/config'
+import { admin } from '../../access/admin'
+import { adminOrPublished } from '../../access/adminOrPublished'
 import { Archive } from '../../blocks/ArchiveBlock/config'
 import { CallToAction } from '../../blocks/CallToAction/config'
 import { Content } from '../../blocks/Content/config'
 import { FormBlock } from '../../blocks/Form/config'
 import { MediaBlock } from '../../blocks/MediaBlock/config'
-import { hero } from '@/heros/config'
-import { slugField } from '@/fields/slug'
 import { populatePublishedAt } from '../../hooks/populatePublishedAt'
 import { generatePreviewPath } from '../../utilities/generatePreviewPath'
 import { revalidateDelete, revalidatePage } from './hooks/revalidatePage'
@@ -24,10 +24,10 @@ import {
 export const Pages: CollectionConfig<'pages'> = {
   slug: 'pages',
   access: {
-    create: authenticated,
-    delete: authenticated,
-    read: authenticatedOrPublished,
-    update: authenticated,
+    create: admin,
+    delete: admin,
+    read: adminOrPublished,
+    update: admin,
   },
   // This config controls what's populated by default when a page is referenced
   // https://payloadcms.com/docs/queries/select#defaultpopulate-collection-config-property
