cba0858d45004d76d4ffd50c42c9c8a0fdbc92cf
Delnegend <kiennguyen19323@gmail.com>
2025-04-18 00:11:05 +0700

    refactor(collections/Categories,Media): rename access fn name to `admin`

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -1,16 +1,16 @@
 import type { CollectionConfig } from 'payload'
 
-import { anyone } from '../access/anyone'
-import { authenticated } from '../access/authenticated'
 import { slugField } from '@/fields/slug'
+import { admin } from '../access/admin'
+import { anyone } from '../access/anyone'
 
 export const Categories: CollectionConfig = {
   slug: 'categories',
   access: {
-    create: authenticated,
-    delete: authenticated,
+    create: admin,
+    delete: admin,
     read: anyone,
-    update: authenticated,
+    update: admin,
   },
   admin: {
     useAsTitle: 'title',
