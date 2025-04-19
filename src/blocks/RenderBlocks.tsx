55b56a5d6af3070af2d87b85844f629e16d929ec
Delnegend <kiennguyen19323@gmail.com>
2025-04-20 00:24:31 +0700

    refactor(blocks/RenderBlocks): early returns

--- src/blocks/RenderBlocks.tsx
+++ src/blocks/RenderBlocks.tsx
@@ -23,31 +23,30 @@ export const RenderBlocks: React.FC<{
 }> = (props) => {
   const { blocks } = props
 
-  const hasBlocks = blocks && Array.isArray(blocks) && blocks.length > 0
-
-  if (hasBlocks) {
-    return (
-      <Fragment>
-        {blocks.map((block, index) => {
-          const { blockType } = block
-
-          if (blockType && blockType in blockComponents) {
-            const Block = blockComponents[blockType]
-
-            if (Block) {
-              return (
-                <div className="my-16" key={index}>
-                  {/* @ts-expect-error there may be some mismatch between the expected types here */}
-                  <Block {...block} disableInnerContainer />
-                </div>
-              )
-            }
-          }
-          return null
-        })}
-      </Fragment>
-    )
+  if (!(blocks && Array.isArray(blocks) && blocks.length > 0)) {
+    return null
   }
 
-  return null
+  return (
+    <Fragment>
+      {blocks.map((block, index) => {
+        const { blockType } = block
+        if (!(blockType && blockType in blockComponents)) {
+          return null
+        }
+
+        const Block = blockComponents[blockType]
+        if (!Block) {
+          return null
+        }
+
+        return (
+          <div className="my-16" key={index}>
+            {/* @ts-expect-error there may be some mismatch between the expected types here */}
+            <Block {...block} disableInnerContainer />
+          </div>
+        )
+      })}
+    </Fragment>
+  )
 }
