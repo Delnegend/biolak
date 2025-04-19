ea234bb5309ca234487fa7de672dc83e043fb2c0
Delnegend <kiennguyen19323@gmail.com>
2025-04-19 11:54:56 +0700

    feat(`globals`): `ContactForm`

---
+++ src/globals/ContactForm/hooks/revalidateContactForm.ts
@@ -0,0 +1,15 @@
+import { revalidateTag } from 'next/cache'
+import { GlobalAfterChangeHook } from 'payload'
+
+export const revalidateContactForm: GlobalAfterChangeHook = ({
+  doc,
+  req: { payload, context },
+}) => {
+  if (!context.disableRevalidate) {
+    payload.logger.info(`Revalidating contact form`)
+
+    revalidateTag('global_contact-form')
+  }
+
+  return doc
+}
