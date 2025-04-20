972ec9ed32eaeab36e62cd0ad34fbdcb505bf1fd
Delnegend <kiennguyen19323@gmail.com>
2025-04-20 12:28:26 +0700

    feat(`globals/Footer/Component`): restyle; use new fields

--- src/globals/Footer/Component.tsx
+++ src/globals/Footer/Component.tsx
@@ -1,29 +1,70 @@
 import { getCachedGlobal } from '@/utilities/getGlobals'
-import Link from 'next/link'
 
+import { TextInput } from '@/components/ui/text-input'
 import type { Footer } from '@/payload-types'
+import { cn } from '@/utilities/ui'
+import { Lato, Phudu } from 'next/font/google'
+import Image from 'next/image'
+import { DataFromGlobalSlug } from 'payload'
 
-import { CMSLink } from '@/components/Link'
-import { Logo } from '@/components/Logo/Logo'
+const lato = Lato({ subsets: ['latin'], weight: ['400'] })
+const phudu = Phudu({ subsets: ['vietnamese'], weight: ['400', '600', '700'] })
 
 export async function Footer() {
-  const footerData: Footer = await getCachedGlobal('footer', 1)()
+  const {
+    contactUs,
+    legal,
+    image: images,
+  }: Footer = (await getCachedGlobal('footer', 1)()) as DataFromGlobalSlug<'footer'>
 
-  const navItems = footerData?.navItems || []
+  const image =
+    images !== undefined && typeof images.image === 'object' && images.image !== null
+      ? images.image
+      : undefined
+
+  const stamp =
+    legal.stamp !== undefined && typeof legal.stamp === 'object' && legal.stamp !== null
+      ? legal.stamp
+      : undefined
 
   return (
-    <footer className="mt-auto border-t border-border bg-black dark:bg-card text-white">
-      <div className="container py-8 gap-8 flex flex-col md:flex-row md:justify-between">
-        <Link className="flex items-center" href="/">
-          <Logo />
-        </Link>
+    <footer className="flex relative overflow-hidden">
+      <Image
+        src={image?.url ?? 'https://placehold.co/1000x1000'}
+        alt={image?.alt ?? 'Lanscape'}
+        width={image?.width ?? 1000}
+        height={image?.height ?? 1000}
+        className="object-cover w-1/2 absolute h-full top-1/2 -translate-y-1/2"
+        unoptimized={image?.url === undefined}
+      />
 
-        <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
-          <nav className="flex flex-col md:flex-row gap-4">
-            {navItems.map(({ link }, i) => {
-              return <CMSLink className="text-white" key={i} {...link} />
-            })}
-          </nav>
+      <div className="w-1/2 translate-x-full">
+        <div className="px-28 py-[5.25rem] flex flex-col gap-6">
+          {/* contact us */}
+          <div className="text-7xl font-medium font-serif">{contactUs.title}</div>
+          <TextInput label={contactUs.emailInputLabel} required />
+          <div className={cn('text-xl font-normal text-[#4B2711] leading-8', lato.className)}>
+            {contactUs.description}
+          </div>
+        </div>
+
+        {/* legal stuffs */}
+        <div className={cn('bg-[#210E0A] px-24 py-9 text-[#F1DAAE]', phudu.className)}>
+          <div className="text-2xl uppercase mb-6 leading-6 font-bold">{legal.title}</div>
+          <div className={cn('text-base uppercase whitespace-pre font-normal', lato.className)}>
+            {legal.content}
+          </div>
+
+          <Image
+            src={stamp?.url ?? 'https://placehold.co/200x100'}
+            alt={stamp?.alt ?? 'Đã thông báo bộ Công Thương'}
+            width={stamp?.width ?? 200}
+            height={stamp?.height ?? 100}
+            className="object-contain my-6 max-h-14 w-fit"
+            unoptimized={stamp?.url === undefined}
+          />
+
+          <div className="text-xs font-normal text-[#F1DAAE]">{legal.copyright}</div>
         </div>
       </div>
     </footer>
