472ee46f555343142e3de42bcb3916c23107b303
Delnegend <kiennguyen19323@gmail.com>
2025-04-20 12:27:21 +0700

    feat(`heros/LowImpact/index`): use media; restyle

--- src/heros/LowImpact/index.tsx
+++ src/heros/LowImpact/index.tsx
@@ -1,25 +1,44 @@
+import type { Page } from '@/payload-types'
+import Image from 'next/image'
 import React from 'react'
 
-import type { Page } from '@/payload-types'
-
 import RichText from '@/components/RichText'
+import { ArrowDown } from 'lucide-react'
 
-type LowImpactHeroType =
-  | {
-      children?: React.ReactNode
-      richText?: never
+interface ValidMedia {
+  url: string
+  width: number
+  height: number
+  alt: string
+}
+
+export const LowImpactHero: React.FC<Page['hero']> = (props) => {
+  const { richText, media: media_ } = props
+
+  let media: ValidMedia | undefined
+  if (
+    media_ !== undefined &&
+    typeof media_ === 'object' &&
+    media_ !== null &&
+    media_.url &&
+    media_.width &&
+    media_.height
+  ) {
+    media = {
+      url: media_.url,
+      width: media_.width,
+      height: media_.height,
+      alt: media_.alt ?? '',
     }
-  | (Omit<Page['hero'], 'richText'> & {
-      children?: never
-      richText?: Page['hero']['richText']
-    })
+  }
 
-export const LowImpactHero: React.FC<LowImpactHeroType> = ({ children, richText }) => {
   return (
-    <div className="container mt-16">
-      <div className="max-w-[48rem]">
-        {children || (richText && <RichText data={richText} enableGutter={false} />)}
+    <div className="container flex items-center flex-col justify-center h-[22.5rem] gap-11">
+      {media && <Image src={media.url} alt={media.alt} width={media.width} height={media.height} />}
+      <div className="max-w-3xl text-center">
+        {richText && <RichText data={richText} enableGutter={false} />}
       </div>
+      <ArrowDown color="#485A1E" size={30} />
     </div>
   )
 }
