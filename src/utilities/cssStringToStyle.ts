636ef5286ed1eafb1f7895ce0c52d6f606b5b16c
Delnegend <kiennguyen19323@gmail.com>
2025-04-20 12:09:31 +0700

    feat(utils/cssStringToStyle): `kebabCaseToCamelCase`

--- src/utilities/cssStringToStyle.ts
+++ src/utilities/cssStringToStyle.ts
@@ -6,8 +6,15 @@ export function cssStringToStyle(cssString?: string | null): Record<string, stri
   cssString_.split(';').forEach((rule) => {
     const [key, value] = rule.split(':')
     if (!key || !value) return
-    style[key.trim()] = value.trim()
+    style[kebabCaseToCamelCase(key.trim())] = value.trim()
   })
 
   return style
+}
+
+function kebabCaseToCamelCase(str: string): string {
+  return str.replace(/-([a-z])/g, (g) => {
+    if (g[1] === undefined) return ''
+    return g[1].toUpperCase()
+  })
 }
