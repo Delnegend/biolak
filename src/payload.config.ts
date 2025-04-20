16fbb8f2cfaf7b57e0a7a20876e491ac9dbbf88c
Delnegend <kiennguyen19323@gmail.com>
2025-04-20 17:37:40 +0700

    fix(payload.config): disable drizzle auto-push

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -65,6 +65,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URI,
     },
+    push: false,
   }),
   collections: [Pages, Posts, Media, Categories, Users, Products],
   cors: [getServerSideURL()].filter(Boolean),
