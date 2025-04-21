b9b23e429b27ec364fb734e6c92b8deb5ee2a84f
Delnegend <kiennguyen19323@gmail.com>
2025-04-21 19:24:05 +0700

    feat(collections): `ProductSubCategories`

--- src/collections/ProductSubCategories.ts
+++ src/collections/ProductSubCategories.ts
@@ -2,10 +2,11 @@ import { admin } from '@/access/admin'
 import { anyone } from '@/access/anyone'
 import { slugField } from '@/fields/slug'
 import { CollectionConfig } from 'payload'
+import { PRODUCT_CATEGORIES_SLUG } from './ProductCategories'
 
-export const PRODUCT_CATEGORIES_SLUG = 'productCategories'
-export const ProductCategories: CollectionConfig = {
-  slug: PRODUCT_CATEGORIES_SLUG,
+export const PRODUCT_SUB_CATEGORY_SLUG = 'productSubCategories'
+export const ProductSubCategories: CollectionConfig = {
+  slug: PRODUCT_SUB_CATEGORY_SLUG,
   access: {
     create: admin,
     delete: admin,
@@ -13,6 +14,12 @@ export const ProductCategories: CollectionConfig = {
     update: admin,
   },
   fields: [
+    {
+      name: PRODUCT_CATEGORIES_SLUG,
+      type: 'relationship',
+      relationTo: PRODUCT_CATEGORIES_SLUG,
+      required: true,
+    },
     {
       name: 'title',
       type: 'text',
