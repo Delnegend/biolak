f0540f7105a32cc897bc61490d5d18bbde920f81
Delnegend <kiennguyen19323@gmail.com>
2025-04-21 19:11:28 +0700

    feat(heroes/LowImpact) add vertical margin

--- src/heros/LowImpact/index.tsx
+++ src/heros/LowImpact/index.tsx
@@ -33,7 +33,7 @@ export const LowImpactHero: React.FC<Page['hero']> = (props) => {
   }
 
   return (
-    <div className="container flex items-center flex-col justify-center h-[22.5rem] gap-11">
+    <div className="container flex items-center flex-col justify-center h-[22.5rem] gap-11 my-16">
       {media && <Image src={media.url} alt={media.alt} width={media.width} height={media.height} />}
       <div className="max-w-3xl text-center">
         {richText && <RichText data={richText} enableGutter={false} />}
