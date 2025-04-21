56879215dca70254674f5a430f11578e26a1e892
Delnegend <kiennguyen19323@gmail.com>
2025-04-21 21:08:09 +0700

    fix: middleware (again)

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === '/') {
-    return Response.redirect(new URL('/home', request.url).href, 307)
+    return Response.redirect(new URL('/home', request.url), 307)
   }
 }
 
