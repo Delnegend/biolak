e737a55a6fa9652e0308dc1e9ac2e0df9856c610
Delnegend <kiennguyen19323@gmail.com>
2025-04-22 12:50:03 +0700

    chore: autogen, migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250421_115821 from './20250421_115821';
+import * as migration_20250422_054831 from './20250422_054831';
 
 export const migrations = [
   {
     up: migration_20250421_115821.up,
     down: migration_20250421_115821.down,
-    name: '20250421_115821'
+    name: '20250421_115821',
+  },
+  {
+    up: migration_20250422_054831.up,
+    down: migration_20250422_054831.down,
+    name: '20250422_054831'
   },
 ];
