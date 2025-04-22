593ccf42f08930570315bb68e9924644946d32d1
Delnegend <kiennguyen19323@gmail.com>
2025-04-22 22:05:51 +0700

    refactor(globals/Header,Footer/hooks/revalidate*): use slug

--- src/globals/Footer/hooks/revalidateFooter.ts
+++ src/globals/Footer/hooks/revalidateFooter.ts
@@ -1,12 +1,13 @@
 import type { GlobalAfterChangeHook } from 'payload'
 
 import { revalidateTag } from 'next/cache'
+import { Footer } from '../config'
 
 export const revalidateFooter: GlobalAfterChangeHook = ({ doc, req: { payload, context } }) => {
   if (!context.disableRevalidate) {
     payload.logger.info(`Revalidating footer`)
 
-    revalidateTag('global_footer')
+    revalidateTag(`global_${Footer.slug}`)
   }
 
   return doc
