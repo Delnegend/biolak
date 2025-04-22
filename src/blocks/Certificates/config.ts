aff0542cb62ef8dc1d7b1ccb9c71ac79afb22ab0
Delnegend <kiennguyen19323@gmail.com>
2025-04-22 21:19:15 +0700

    feat(blocks): `Certificates`

---
+++ src/blocks/Certificates/config.ts
@@ -0,0 +1,37 @@
+import { Media } from '@/collections/Media'
+import { Block, CollectionSlug } from 'payload'
+
+export const CertificatesBlockConfig: Block = {
+  slug: 'certificates',
+  interfaceName: 'CertificatesBlockProps',
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+      defaultValue: 'Chứng nhận bởi các tổ chức quốc tế',
+    },
+    {
+      type: 'array',
+      name: 'organizations',
+      maxRows: 3,
+      fields: [
+        {
+          name: 'title',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'description',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'logo',
+          type: 'upload',
+          relationTo: Media.slug as CollectionSlug,
+        },
+      ],
+    },
+  ],
+}
