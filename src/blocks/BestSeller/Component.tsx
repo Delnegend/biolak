5f75ed8e25773b856bef8bf8257339fe289126e5
Delnegend <kiennguyen19323@gmail.com>
2025-04-26 10:12:03 +0700

    feat(blocks): `BestSeller`

---
+++ src/blocks/BestSeller/Component.tsx
@@ -0,0 +1,88 @@
+import { Button } from '@/components/ui/button'
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel'
+import { BestSellerBlockProps } from '@/payload-types'
+import { formatPrice } from '@/utilities/formatPrice'
+import { cn } from '@/utilities/ui'
+import { CirclePlus } from 'lucide-react'
+import { Lato } from 'next/font/google'
+import Image from 'next/image'
+import React from 'react'
+
+const lato = Lato({
+  subsets: ['latin'],
+  weight: ['400'],
+})
+
+export function BestSellerBlock(props: BestSellerBlockProps): React.JSX.Element {
+  const products =
+    props.products
+      ?.map((p) => p !== null && p.product)
+      .filter((p) => typeof p === 'object' && p !== null) || []
+
+  return (
+    <div className="flex h-[48rem]">
+      <div className="flex h-full w-[40rem] flex-col justify-end gap-9 px-16 py-24">
+        <div
+          className={cn('font-serif text-7xl font-semibold italic leading-[3.5rem] text-primary')}
+        >
+          {props.title}
+        </div>
+        {props.description && <div className={cn('text-balance')}>{props.description}</div>}
+      </div>
+      <Carousel opts={{ dragFree: true }} className="w-full py-24">
+        <CarouselContent>
+          {products.map((p) => {
+            const img =
+              p.gallery &&
+              p.gallery.length > 0 &&
+              typeof p.gallery[0]?.image === 'object' &&
+              p.gallery[0]?.image !== null
+                ? p.gallery[0]?.image
+                : null
+            return (
+              <CarouselItem
+                key={p.id}
+                className="grid w-full max-w-96 grid-cols-[1fr_auto] grid-rows-[auto]"
+                style={{
+                  gridTemplateAreas: `"img img"
+                                    "title add-to-cart"
+                                    "desc add-to-cart"
+                                    "price add-to-cart"`,
+                }}
+              >
+                <Image
+                  src={img?.url ?? 'https://placehold.co/500x500'}
+                  alt={`Ảnh sản phẩm ${p.title}`}
+                  width={img?.width ?? 500}
+                  height={img?.height ?? 500}
+                  style={{ gridArea: 'img' }}
+                  unoptimized={img === null}
+                  className="mb-6 rounded-[0.5rem]"
+                />
+                <div style={{ gridArea: 'title' }} className="mb-2 text-lg font-bold">
+                  {`${p.title} →`}
+                </div>
+                <div
+                  style={{ gridArea: 'desc' }}
+                  className={cn('mb-[1.375rem] text-xs uppercase', lato.className)}
+                >
+                  {p.shortDescription}
+                </div>
+                <div style={{ gridArea: 'price' }}>{formatPrice(p.price)}</div>
+                <div style={{ gridArea: 'add-to-cart' }}>
+                  <Button
+                    hideArrow={true}
+                    className="flex size-12 items-center justify-center rounded-[0.5rem] bg-[#E7B27E] p-0 hover:bg-[#ca9c6e]"
+                    title="Thêm vào giỏ hàng"
+                  >
+                    <CirclePlus className="w-full" />
+                  </Button>
+                </div>
+              </CarouselItem>
+            )
+          })}
+        </CarouselContent>
+      </Carousel>
+    </div>
+  )
+}
