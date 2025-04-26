3b2b050edbacfa5f6161d39ea8e25e43e0ff911b
Delnegend <kiennguyen19323@gmail.com>
2025-04-26 10:12:30 +0700

    feat(utils): format price

---
+++ src/utilities/formatPrice.ts
@@ -0,0 +1,8 @@
+export function formatPrice(price: number): string {
+  return price.toLocaleString('vi-VN', {
+    style: 'currency',
+    currency: 'VND',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  })
+}
