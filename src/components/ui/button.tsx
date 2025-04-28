9f353efd65ea350b3686850ebb45babd7706ac4a
Delnegend <kiennguyen19323@gmail.com>
2025-04-28 20:32:16 +0700

    feat(`components/ui/button`): restyle

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -24,7 +24,8 @@ export const buttonVariants = cva(
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         ghost: 'hover:bg-card hover:text-accent-foreground',
         link: 'text-primary items-start justify-start underline-offset-4 hover:underline',
-        outline: 'border border-border bg-background hover:bg-card hover:text-accent-foreground',
+        outline:
+          'border text-[#E7B27E] border-[#E7B27E] bg-background hover:border-black hover:text-black',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
       },
     },
