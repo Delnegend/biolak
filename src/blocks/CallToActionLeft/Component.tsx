57329b1c3b2f147298010fe8dfb7615fe267d7ff
Delnegend <kiennguyen19323@gmail.com>
2025-04-29 23:30:09 +0700

    feat: safe width

--- src/blocks/CallToActionLeft/Component.tsx
+++ src/blocks/CallToActionLeft/Component.tsx
@@ -21,25 +21,27 @@ export function CallToActionLeftBlock(props: CallToActionLeftBlockProps): React.
         backgroundRepeat: 'no-repeat',
       }}
     >
-      <div className="box-content flex w-5/6 max-w-[28rem] flex-col gap-6 p-24">
-        <div className="leading whitespace-pre-wrap font-serif text-8xl font-bold leading-[5rem] text-white">
-          {props.title}
+      <div className="safe-width">
+        <div className="box-content flex max-w-[28rem] flex-col gap-6 py-24 pr-24">
+          <div className="leading whitespace-pre-wrap font-serif text-8xl font-bold leading-[5rem] text-white">
+            {props.title}
+          </div>
+          {props.description && (
+            <RichText
+              data={props.description}
+              enableGutter={false}
+              className="mx-0 text-balance text-xl leading-8 text-white"
+            />
+          )}
+          <Link
+            href={props.button.link?.url ?? '#'}
+            target={props.button.link?.newTab ? '_blank' : '_self'}
+          >
+            <Button className="w-full" size="lg">
+              {props.button.text}
+            </Button>
+          </Link>
         </div>
-        {props.description && (
-          <RichText
-            data={props.description}
-            enableGutter={false}
-            className="text-balance text-xl leading-8 text-white"
-          />
-        )}
-        <Link
-          href={props.button.link?.url ?? '#'}
-          target={props.button.link?.newTab ? '_blank' : '_self'}
-        >
-          <Button className="w-full" size="lg">
-            {props.button.text}
-          </Button>
-        </Link>
       </div>
     </div>
   )
