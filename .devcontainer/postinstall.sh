688a94b7d08fafc589e38ff78729934c066d40da
Delnegend <kiennguyen19323@gmail.com>
2025-05-02 15:56:43 +0700

    build(.devcontainer/postinstall): add checksum

--- .devcontainer/postinstall.sh
+++ .devcontainer/postinstall.sh
@@ -2,15 +2,31 @@
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-JUST_VERSION=1.40.0 && sudo rm -f /usr/local/bin/just && \
-    curl -sL https://github.com/casey/just/releases/download/$JUST_VERSION/just-$JUST_VERSION-x86_64-unknown-linux-musl.tar.gz |\
-    sudo tar -xzf - -C /usr/local/bin just && \
-    [[ -f /usr/local/bin/just ]] && echo "just is installed" || echo "just is NOT installed"
+ver=1.40.0 && sudo rm -f /usr/local/bin/just
+curl -s -L -o /tmp/just.tar.gz https://github.com/casey/just/releases/download/$ver/just-$ver-x86_64-unknown-linux-musl.tar.gz
+checksum=$(openssl dgst -sha3-512 /tmp/just.tar.gz | awk '{print $2}')
+expected="27f317b6ca704395dbad34c078f57d140d6d3e1f147a29b5c7563489885525a203bb289b02ec05e52282d25dfe446a60e11554a58addc682ad17f94b6b100cb9"
+if [ "$checksum" != "$expected" ]; then
+  echo "just checksum mismatch, expected $expected, got $checksum"
+else
+  sudo rm -f /usr/local/bin/just
+  sudo tar -xzf /tmp/just.tar.gz -C /usr/local/bin just
+  [[ -f "/usr/local/bin/just" ]] && echo "just installed successfully"
+fi
+rm -f /tmp/just.tar.gz
 
-FZF_VERSION=0.61.3 && sudo rm -f /usr/local/bin/fzf && \
-    curl -sL https://github.com/junegunn/fzf/releases/download/v$FZF_VERSION/fzf-$FZF_VERSION-linux_amd64.tar.gz |\
-    sudo tar -xzf - -C /usr/local/bin fzf && \
-    [[ -f /usr/local/bin/fzf ]] && echo "fzf is installed" || echo "fzf is NOT installed"
+ver="0.61.3"
+curl -s -L -o /tmp/fzf.tar.gz https://github.com/junegunn/fzf/releases/download/v$ver/fzf-$ver-linux_amd64.tar.gz
+checksum=$(openssl dgst -sha3-512 /tmp/fzf.tar.gz | awk '{print $2}')
+expected="1710205b6f924c78ebfc6b43e1697e4cf4ba168d7970196f23effb4f125e956a76a07ae8a26dfcd1a4a5b26435b2670bb840b7d1c4ea92befef09789d17068b0"
+if [ "$checksum" != "$expected" ]; then
+  echo "fzf checksum mismatch, expected $expected, got $checksum"
+else
+  sudo rm -f /usr/local/bin/fzf
+  sudo tar -xzf /tmp/fzf.tar.gz -C /usr/local/bin fzf
+  [[ -f "/usr/local/bin/fzf" ]] && echo "fzf installed successfully"
+fi
+rm -f /tmp/fzf.tar.gz
 
 echo 'alias j=just' >> ~/.zshrc
 
