0c3badc0a89d3ad9621db861478dd3a8da58aef7
Delnegend <kiennguyen19323@gmail.com>
2025-05-03 15:46:17 +0700

    chore(postinstall): iptables fix

--- .devcontainer/postinstall.sh
+++ .devcontainer/postinstall.sh
@@ -35,3 +35,5 @@ export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
 volta install node@lts pnpm
 pnpm config set store-dir ~/.pnpm-store
+
+echo 0 | sudo update-alternatives --config iptables
