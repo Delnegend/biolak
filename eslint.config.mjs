7c8deb41183b9a10224075d8921c0064ee86c3c6
Delnegend <kiennguyen19323@gmail.com>
2025-05-05 16:13:39 +0700

    build(`eslint.config`): `simpleImportSort`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { FlatCompat } from '@eslint/eslintrc'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -11,6 +12,15 @@ const compat = new FlatCompat({
 
 export default [
 	...compat.extends('next/core-web-vitals', 'next/typescript'),
+	{
+		plugins: {
+			'simple-import-sort': simpleImportSort,
+		},
+		rules: {
+			'simple-import-sort/imports': 'error',
+			'simple-import-sort/exports': 'error',
+		},
+	},
 	{
 		rules: {
 			'@typescript-eslint/ban-ts-comment': 'warn',
