cd746b9af1941bf855be3a91a5a6d60adb663dfb
Delnegend <kiennguyen19323@gmail.com>
2025-05-05 19:36:20 +0700

    feat(components): `ProductCard`

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,72 @@
+import { CirclePlus } from 'lucide-react'
+import { Lato } from 'next/font/google'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Product } from '@/payload-types'
+import { formatPrice } from '@/utilities/formatPrice'
+import { cn } from '@/utilities/ui'
+
+import { Button } from './ui/button'
+
+const lato = Lato({
+	weight: ['400'],
+})
+
+export function ProductCard({
+	product: p,
+	component,
+}: {
+	product: Product
+	component?: React.ElementType
+}): React.JSX.Element {
+	const Comp = component ?? 'div'
+	const img =
+		p.gallery && p.gallery[0] && p.gallery[0].image && typeof p.gallery[0].image === 'object'
+			? p.gallery[0].image
+			: null
+
+	return (
+		<Comp
+			key={p.id}
+			className="grid max-w-96 grid-cols-[1fr_auto] grid-rows-[auto]"
+			style={{
+				gridTemplateAreas: `"img img"
+											"title add-to-cart"
+											"desc add-to-cart"
+											"price add-to-cart"`,
+			}}
+		>
+			<Link href={p.slug ? `/products/${p.slug}` : '#'} style={{ gridArea: 'img' }}>
+				<Image
+					src={img?.url ?? 'https://placehold.co/1000x1000'}
+					alt={`Ảnh sản phẩm ${p.title}`}
+					width={img?.width ?? 1000}
+					height={img?.height ?? 1000}
+					style={{ gridArea: 'img' }}
+					unoptimized={img === null}
+					className="mb-6 rounded-[0.5rem]"
+				/>
+			</Link>
+			<div style={{ gridArea: 'title' }} className="mb-2 text-lg font-bold">
+				{`${p.title} →`}
+			</div>
+			<div
+				style={{ gridArea: 'desc' }}
+				className={cn('mb-[1.375rem] text-xs uppercase', lato.className)}
+			>
+				{p.shortDescription}
+			</div>
+			<div style={{ gridArea: 'price' }}>{formatPrice(p.price)}</div>
+			<div style={{ gridArea: 'add-to-cart' }} className="flex items-center justify-end">
+				<Button
+					hideArrow={true}
+					className="group flex size-12 items-center justify-center rounded-[0.5rem] border-[#E7B27E] bg-[#E7B27E] p-0 transition-all hover:border hover:bg-transparent"
+					title="Thêm vào giỏ hàng"
+				>
+					<CirclePlus className="w-full transition-colors group-hover:text-[#E7B27E]" />
+				</Button>
+			</div>
+		</Comp>
+	)
+}
