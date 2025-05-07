8992c1ce6580b61e38c88df3bb702874dfc7947f
Delnegend <kiennguyen19323@gmail.com>
2025-05-07 13:02:28 +0700

    feat(`components`): `PostCard`

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Post } from '@/payload-types'
+
+import { CarouselItem } from './ui/carousel'
+
+export function PostCard(post: Post): React.JSX.Element {
+	const img = post.heroImage && typeof post.heroImage === 'object' ? post.heroImage : null
+
+	const author = typeof post.authors?.[0] === 'object' ? post.authors[0] : null
+	const lastModified = typeof post.updatedAt === 'string' ? new Date(post.updatedAt) : new Date()
+	const lastModifiedStr = `${lastModified.getDate().toString().padStart(2, '0')}.${lastModified.getMonth().toString().padStart(2, '0')}.${lastModified.getFullYear().toString().slice(2)}`
+
+	return (
+		<Link href={`/posts/${post.slug}`}>
+			<CarouselItem className="max-w-[25rem]">
+				<Image
+					src={img?.url || 'https://placehold.co/460x400'}
+					alt={img?.alt || ''}
+					width={img?.width || 0}
+					height={img?.height || 0}
+					className="h-[25rem] w-[28.75rem] rounded-[0.5rem] object-cover"
+					unoptimized={img === null}
+				/>
+
+				<div className="my-6 text-primary">
+					<div className="text-[0.625rem] font-medium">
+						{author?.name ?? 'BioLAK'}
+						&nbsp;|&nbsp;
+						<span className="text-[#e7b27e]">{lastModifiedStr}</span>
+					</div>
+					<div className="font-serif text-2xl font-bold">{post.title}&nbsp;→</div>
+					<div className="text-xs">
+						{post.meta?.description ??
+							'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in nisl aliquam, pharetra diam consequat, tincidunt nibh.'}
+					</div>
+				</div>
+			</CarouselItem>
+		</Link>
+	)
+}
