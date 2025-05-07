ca1b79a0d8d0ab7a06e864cf8295117422f762a7
Delnegend <kiennguyen19323@gmail.com>
2025-05-08 02:18:45 +0700

    feat(`blocks/Certificates`): i18n

--- src/blocks/Certificates/config.ts
+++ src/blocks/Certificates/config.ts
@@ -20,12 +20,30 @@ export const CertificatesBlockConf: Block = {
 		{
 			name: 'title',
 			type: 'text',
+			label: {
+				en: 'Title',
+				vi: 'Tiêu đề',
+			},
 			required: true,
 			defaultValue: 'Chứng nhận bởi các tổ chức quốc tế',
 		},
 		{
 			type: 'array',
 			name: 'organizations',
+			label: {
+				en: 'Organizations',
+				vi: 'Tổ chức',
+			},
+			labels: {
+				singular: {
+					en: 'Organization',
+					vi: 'Tổ chức',
+				},
+				plural: {
+					en: 'Organizations',
+					vi: 'Tổ chức',
+				},
+			},
 			fields: [
 				{
 					name: 'title',
