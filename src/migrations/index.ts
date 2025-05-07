5cf25654261a598ef2428de2ceb4230df2c831b2
Delnegend <kiennguyen19323@gmail.com>
2025-05-07 21:17:02 +0700

    chore: migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250507_114238 from './20250507_114238';
+import * as migration_20250507_141645 from './20250507_141645';
 
 export const migrations = [
   {
     up: migration_20250507_114238.up,
     down: migration_20250507_114238.down,
-    name: '20250507_114238'
+    name: '20250507_114238',
+  },
+  {
+    up: migration_20250507_141645.up,
+    down: migration_20250507_141645.down,
+    name: '20250507_141645'
   },
 ];
