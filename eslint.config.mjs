a5abef14a4c8fad066a7253998727ed47e8c1edf
Delnegend <kiennguyen19323@gmail.com>
2025-05-07 18:54:19 +0700

    chore(eslint.config): ignore importmap file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,7 @@ export default [
 			'simple-import-sort/imports': 'error',
 			'simple-import-sort/exports': 'error',
 		},
+		ignores: ['./src/app/(payload)/admin/importMap.js'],
 	},
 	{
 		rules: {
