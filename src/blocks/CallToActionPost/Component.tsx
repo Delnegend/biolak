a13285fdb9f7b324dc82089434e14bcf21591817
Delnegend <kiennguyen19323@gmail.com>
2025-05-07 18:06:31 +0700

    feat(`blocks`): `CallToActionPost`, `PostsGrid`

---
+++ src/blocks/CallToActionPost/Component.tsx
@@ -0,0 +1,41 @@
+import { Lato } from 'next/font/google'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+import { CallToActionPostBlockProps } from '@/payload-types'
+import { cn } from '@/utilities/ui'
+
+const lato = Lato({ weight: ['400', '700'] })
+
+export function CallToActionPostBlock(props: CallToActionPostBlockProps): React.JSX.Element {
+	const post = props.post && typeof props.post === 'object' ? props.post : null
+
+	const img = post?.heroImage && typeof post.heroImage === 'object' ? post.heroImage : null
+
+	return (
+		<div className="flex h-full flex-row max-lg:flex-col">
+			<div className="flex w-full flex-col gap-[3.75rem] px-[6.125rem] py-[7.125rem] lg:max-w-[36rem]">
+				<div className="font-serif text-[4rem] font-semibold leading-none">
+					{props.overwriteTitle ?? post?.title}
+				</div>
+				<div className={cn('text-xl font-semibold', lato.className)}>
+					{props.overwriteDescription ?? post?.meta?.description}
+				</div>
+				<Link href={post?.slug ? `/posts/${post.slug}` : '#'} className="w-full">
+					<Button size="lg">{props.buttonLabel}</Button>
+				</Link>
+			</div>
+			<div className="w-full lg:relative">
+				<Image
+					src={img?.url ?? 'https://placehold.co/1000x1000'}
+					width={img?.width ?? 1000}
+					height={img?.height ?? 1000}
+					alt={img?.alt ?? ''}
+					unoptimized={img === null}
+					className="size-full object-cover max-lg:aspect-square lg:absolute"
+				/>
+			</div>
+		</div>
+	)
+}
---
+++ src/blocks/PostsGrid/Component.tsx
@@ -0,0 +1,14 @@
+import { PostCard } from '@/components/PostCard'
+import { PostsGridBlockProps } from '@/payload-types'
+
+export function PostsGridBlock(props: PostsGridBlockProps): React.JSX.Element {
+	const posts = props.posts?.filter((post) => typeof post === 'object') ?? []
+
+	return (
+		<div className="safe-width my-20 flex flex-row flex-wrap justify-between">
+			{posts.map((p) => {
+				return <PostCard key={p.slug} post={p} />
+			})}
+		</div>
+	)
+}
