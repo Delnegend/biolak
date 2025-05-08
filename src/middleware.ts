17676c1446cdab34d8589c60361a6b4328697c1b
Delnegend <kiennguyen19323@gmail.com>
2025-05-08 11:23:17 +0700

    feat(`middleware`): redirects `/posts` -> `/events`

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,17 @@ export function middleware(request: NextRequest) {
 	if (request.nextUrl.pathname === '/') {
 		return Response.redirect(new URL('/home', request.url), 307)
 	}
+
+	switch (request.nextUrl.pathname) {
+		case '/':
+			return Response.redirect(new URL('/home', request.url), 307)
+		case '/posts':
+			return Response.redirect(new URL('/events', request.url), 307)
+		default:
+			return undefined
+	}
 }
 
 export const config = {
-	matcher: '/:path*',
+	matcher: ['/', '/posts'],
 }
