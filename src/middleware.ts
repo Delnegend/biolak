559568551b1d92f53d6b10326f3f5d63012d8915
Delnegend <kiennguyen19323@gmail.com>
2025-05-11 00:07:41 +0700

    feat(`middleware`): add request path to header

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,23 @@
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
-	if (request.nextUrl.pathname === '/') {
-		return Response.redirect(new URL('/home', request.url), 307)
-	}
-
 	switch (request.nextUrl.pathname) {
 		case '/':
 			return Response.redirect(new URL('/home', request.url), 307)
 		case '/posts':
 			return Response.redirect(new URL('/events', request.url), 307)
 		default:
-			return undefined
+			// return undefined
+			const headers = new Headers(request.headers)
+			headers.set('x-current-path', request.nextUrl.pathname)
+			return NextResponse.next({
+				request: {
+					headers,
+				},
+			})
 	}
 }
 
 export const config = {
-	matcher: ['/', '/posts'],
+	matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
