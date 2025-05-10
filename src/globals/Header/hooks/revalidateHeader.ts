3dd3c3cf52a940df6b8078700513c7b48c510982
Delnegend <kiennguyen19323@gmail.com>
2025-05-10 16:22:29 +0700

    refactor`globals(): rename config consts

--- src/globals/Header/hooks/revalidateHeader.ts
+++ src/globals/Header/hooks/revalidateHeader.ts
@@ -1,13 +1,13 @@
 import { revalidateTag } from 'next/cache'
 import type { GlobalAfterChangeHook } from 'payload'
 
-import { Header } from '../config'
+import { HeaderGlobalConf } from '../config'
 
 export const revalidateHeader: GlobalAfterChangeHook = ({ doc, req: { payload, context } }) => {
 	if (!context.disableRevalidate) {
 		payload.logger.info(`Revalidating header`)
 
-		revalidateTag(`global_${Header.slug}`)
+		revalidateTag(`global_${HeaderGlobalConf.slug}`)
 	}
 
 	return doc
