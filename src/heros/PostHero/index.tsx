97a76298a923bb3a8d6908e18cf2553032b56d9c
Delnegend <kiennguyen19323@gmail.com>
2025-05-11 17:10:58 +0700

    feat(hero/PostHero): simplify

--- src/heros/PostHero/index.tsx
+++ src/heros/PostHero/index.tsx
@@ -1,9 +1,14 @@
+import { Phudu } from 'next/font/google'
 import React from 'react'
-import { formatDateTime } from 'src/utilities/formatDateTime'
 
-import { Media } from '@/components/Media'
 import type { Post } from '@/payload-types'
 import { formatAuthors } from '@/utilities/formatAuthors'
+import { cn } from '@/utilities/ui'
+
+const phudu = Phudu({
+	subsets: ['vietnamese'],
+	weight: ['600'],
+})
 
 export function PostHero({ post: p }: { post: Post }): React.JSX.Element {
 	const hasAuthors =
@@ -12,58 +17,9 @@ export function PostHero({ post: p }: { post: Post }): React.JSX.Element {
 		formatAuthors(p.populatedAuthors) !== ''
 
 	return (
-		<div className="relative -mt-[10.4rem] flex items-end">
-			<div className="container relative z-10 pb-8 text-white lg:grid lg:grid-cols-[1fr_48rem_1fr]">
-				<div className="col-span-1 col-start-1 md:col-span-2 md:col-start-2">
-					<div className="mb-6 text-sm uppercase">
-						{p.postCategories?.map((category, index) => {
-							if (typeof category === 'object' && category !== null) {
-								const { title: categoryTitle } = category
-
-								const titleToUse = categoryTitle || 'Untitled category'
-
-								const isLast = index === (p?.postCategories?.length ?? 0) - 1
-
-								return (
-									<React.Fragment key={index}>
-										{titleToUse}
-										{!isLast && <React.Fragment>, &nbsp;</React.Fragment>}
-									</React.Fragment>
-								)
-							}
-							return null
-						})}
-					</div>
-
-					<div className="">
-						<h1 className="mb-6 text-3xl md:text-5xl lg:text-6xl">{p.title}</h1>
-					</div>
-
-					<div className="flex flex-col gap-4 md:flex-row md:gap-16">
-						{hasAuthors && (
-							<div className="flex flex-col gap-4">
-								<div className="flex flex-col gap-1">
-									<p className="text-sm">Author</p>
-
-									<p>{formatAuthors(p.populatedAuthors!)}</p>
-								</div>
-							</div>
-						)}
-						{p.publishedAt && (
-							<div className="flex flex-col gap-1">
-								<p className="text-sm">Date Published</p>
-								<time dateTime={p.publishedAt}>{formatDateTime(p.publishedAt)}</time>
-							</div>
-						)}
-					</div>
-				</div>
-			</div>
-			<div className="min-h-[80vh] select-none">
-				{p.heroImage && typeof p.heroImage !== 'string' && (
-					<Media fill priority imgClassName="-z-10 object-cover" resource={p.heroImage} />
-				)}
-				<div className="pointer-events-none absolute bottom-0 left-0 h-1/2 w-full bg-gradient-to-t from-black to-transparent" />
-			</div>
+		<div className="safe-width mt-14 text-primary">
+			<div className={cn('mb-7 text-2xl font-medium', phudu.className)}>BÀI VIẾT</div>
+			<h1 className="max-w-[30rem] text-balance">{p.title}</h1>
 		</div>
 	)
 }
