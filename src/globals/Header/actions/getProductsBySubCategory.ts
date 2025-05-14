8a9ce944d2280f164825873e2ca1e015b7e5f435
Delnegend <kiennguyen19323@gmail.com>
2025-05-14 16:33:38 +0700

    refactor(`globals/Headers`): replace custom cache layer w/ react's cache

--- src/globals/Header/actions/getProductsBySubCategory.ts
+++ src/globals/Header/actions/getProductsBySubCategory.ts
@@ -1,23 +1,26 @@
 'use server'
 import configPromise from '@payload-config'
 import { getPayload } from 'payload'
+import { cache } from 'react'
 
 import { ProductsSlug } from '@/collections/Products/slug'
 import { ProductSubCategoriesSlug } from '@/collections/ProductSubCategories/slug'
 import { Product } from '@/payload-types'
 
-export async function getProductsBySubCategory(subCategorySlug: unknown): Promise<Product[]> {
-	if (typeof subCategorySlug !== 'string') throw new Error('subCategorySlug must be a string')
+export const getProductsBySubCategory = cache(
+	async (subCategorySlug: unknown): Promise<Product[]> => {
+		if (typeof subCategorySlug !== 'string') throw new Error('subCategorySlug must be a string')
 
-	const payload = await getPayload({ config: configPromise })
-	const products = await payload.find({
-		collection: ProductsSlug,
-		where: {
-			[`${ProductSubCategoriesSlug}.slug`]: {
-				equals: subCategorySlug,
+		const payload = await getPayload({ config: configPromise })
+		const products = await payload.find({
+			collection: ProductsSlug,
+			where: {
+				[`${ProductSubCategoriesSlug}.slug`]: {
+					equals: subCategorySlug,
+				},
 			},
-		},
-	})
+		})
 
-	return products.docs
-}
+		return products.docs
+	},
+)
