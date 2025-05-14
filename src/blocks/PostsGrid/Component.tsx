17b3a8450e92d3376d1a273e02c1e31216cf71e3
Delnegend <kiennguyen19323@gmail.com>
2025-05-14 15:41:51 +0700

    refactor(blocks/PostsGrid): add gap, justify center

--- src/blocks/PostsGrid/Component.tsx
+++ src/blocks/PostsGrid/Component.tsx
@@ -5,7 +5,7 @@ export function PostsGridBlock(props: PostsGridBlockProps): React.JSX.Element {
 	const posts = props.posts?.filter((post) => typeof post === 'object') ?? []
 
 	return (
-		<div className="safe-width my-20 flex flex-row flex-wrap justify-between">
+		<div className="safe-width my-20 flex flex-row flex-wrap justify-center gap-x-10">
 			{posts.map((p) => {
 				return <PostCard key={p.slug} post={p} />
 			})}
