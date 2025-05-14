b739a4d3b1d4a0b26b0a642451da77b5c5bdd341
Delnegend <kiennguyen19323@gmail.com>
2025-05-14 16:49:41 +0700

    feat(`globals/Header/ProductsDropdown`): reusable comps; restyle; product icon

--- src/globals/Header/components/ProductsDropdown.client.tsx
+++ src/globals/Header/components/ProductsDropdown.client.tsx
@@ -1,9 +1,13 @@
 'use client'
 import { AnimatePresence, motion, Variants } from 'motion/react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { PaginatedDocs } from 'payload'
-import { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import { Product, ProductCategory, ProductSubCategory } from '@/payload-types'
+import { formatPrice } from '@/utilities/formatPrice'
 import { cn } from '@/utilities/ui'
 
 import { getProductsBySubCategory } from '../actions/getProductsBySubCategory'
@@ -26,6 +30,67 @@ const itemAnimationVariants: Variants = {
 		x: -30,
 		transition: { duration: 0.2, ease: 'easeInOut' },
 	},
+}
+
+function DropdownLabel({
+	children,
+	className,
+	...props
+}: React.ComponentPropsWithRef<typeof motion.div> & {
+	className?: string
+}): React.JSX.Element {
+	return (
+		<motion.div
+			variants={panelAnimationVariants}
+			initial="initial"
+			animate="animate"
+			exit="exit"
+			className={cn('mb-1 text-base font-bold text-primary', className)}
+			custom={0}
+			{...props}
+		>
+			{children}
+		</motion.div>
+	)
+}
+
+function DropdownItem({
+	children,
+	className,
+	...props
+}: React.ComponentPropsWithRef<typeof motion.button>): React.JSX.Element {
+	return (
+		<motion.button
+			variants={itemAnimationVariants}
+			initial="initial"
+			animate="animate"
+			exit="exit"
+			className={cn(
+				'hover-underline-animation w-fit whitespace-nowrap text-start text-2xl font-medium',
+				className,
+			)}
+			{...props}
+		>
+			{children}
+		</motion.button>
+	)
+}
+
+function DropdownColumn({
+	children,
+	className,
+}: React.ComponentPropsWithRef<typeof motion.div>): React.JSX.Element {
+	return (
+		<motion.div
+			variants={panelAnimationVariants}
+			initial="initial"
+			animate="animate"
+			exit="exit"
+			className={cn('flex w-1/3 flex-col gap-1 bg-primary-foreground px-20 py-12', className)}
+		>
+			{children}
+		</motion.div>
+	)
 }
 
 export function ProductsDropdownClient({
@@ -89,7 +154,9 @@ export function ProductsDropdownClient({
 						initial="initial"
 						animate="animate"
 						exit="exit"
-						className={cn('absolute left-[-6.5rem] top-[3.25rem] flex w-dvw bg-black/85')}
+						className={cn(
+							'absolute left-[-6.5rem] top-[3.25rem] flex w-dvw border-t bg-black/85',
+						)}
 						onClick={(e) => {
 							if (e.target === dropdownElement.current) {
 								setOpen(false)
@@ -97,119 +164,121 @@ export function ProductsDropdownClient({
 						}}
 					>
 						{/* categories */}
-						<div className="flex flex-col gap-1 bg-primary-foreground px-20 py-12">
-							<motion.div
-								variants={itemAnimationVariants}
-								custom={0}
-								initial="initial"
-								animate="animate"
-								exit="exit"
-								className="whitespace-nowrap text-base font-bold"
-							>
-								Sản phẩm
-							</motion.div>
+						<DropdownColumn className="z-50 border-r" key="categories">
+							<DropdownLabel key="categories">Sản phẩm</DropdownLabel>
 							{categories.docs.map((category, index) => (
-								<motion.button
+								<DropdownItem
 									key={category.slug}
 									custom={index + 1}
-									variants={itemAnimationVariants}
-									initial="initial"
-									animate="animate"
-									exit="exit"
-									className="whitespace-nowrap text-start text-2xl font-medium"
 									onClick={(e) => {
 										e.stopPropagation() // Prevent closing dropdown
 										handleCategoryLoad(category)
 									}}
 									onMouseEnter={() => handleCategoryLoad(category)}
+									className={activeCategory?.slug === category.slug ? 'keep-hover' : ''}
 								>
 									{category.title}
-								</motion.button>
+								</DropdownItem>
 							))}
-						</div>
+						</DropdownColumn>
 
 						{/* sub categories */}
 						<AnimatePresence mode="wait">
-							{activeCategory?.productSubCategories?.docs?.length ? (
-								<motion.div
-									key="subCategoryPanel"
-									variants={panelAnimationVariants}
-									initial="initial"
-									animate="animate"
-									exit="exit"
-									className={cn('flex flex-col gap-1 bg-primary-foreground px-20 py-12')}
-								>
-									<motion.div
-										variants={itemAnimationVariants}
-										custom={0}
-										initial="initial"
-										animate="animate"
-										exit="exit"
-										className="whitespace-nowrap text-base font-bold"
-									>
-										{activeCategory?.title}
-									</motion.div>
+							{activeCategory?.productSubCategories?.docs?.length && (
+								<DropdownColumn className="z-40 border-r" key="subCategoryPanel">
+									<DropdownLabel key={activeCategory?.slug}>
+										{activeCategory?.title ?? 'Danh mục sản phẩm'}
+									</DropdownLabel>
 									{activeCategory?.productSubCategories?.docs
 										?.filter((p) => typeof p === 'object')
 										.map((category, index) => (
-											<motion.button
+											<DropdownItem
 												key={category.slug}
-												custom={index + 1}
-												variants={itemAnimationVariants}
-												initial="initial"
-												animate="animate"
-												exit="exit"
-												className="whitespace-nowrap text-start text-2xl font-medium"
+												custom={index}
 												onClick={(e) => {
 													e.stopPropagation() // Prevent closing dropdown
-													handleSubcategoryLoad(category)
+													handleSubcategoryLoad(category as ProductSubCategory)
 												}}
-												onMouseEnter={() => handleSubcategoryLoad(category)}
+												onMouseEnter={() =>
+													handleSubcategoryLoad(category as ProductSubCategory)
+												}
+												className={
+													activeSubCategory?.slug === category.slug ? 'keep-hover' : ''
+												}
 											>
 												{category.title}
-											</motion.button>
+											</DropdownItem>
 										))}
-								</motion.div>
-							) : null}
+								</DropdownColumn>
+							)}
 						</AnimatePresence>
 
 						{/* products */}
 						<AnimatePresence mode="wait">
-							{(activeProducts?.length ?? 0) > 0 ? (
-								<motion.div
-									key="productsPanel"
-									variants={panelAnimationVariants}
-									initial="initial"
-									animate="animate"
-									exit="exit"
-									className={cn('flex flex-col gap-1 bg-primary-foreground px-20 py-12')}
-								>
-									<motion.div
-										variants={itemAnimationVariants}
-										initial="initial"
-										animate="animate"
-										custom={0}
-										exit="exit"
-										className="text-base font-bold"
-									>
-										{activeSubCategory?.title}
-									</motion.div>
-									{activeProducts?.map((product, index) => (
-										<motion.button
-											key={product.slug}
-											custom={index + 1}
-											variants={itemAnimationVariants}
-											initial="initial"
-											animate="animate"
-											exit="exit"
-											className="text-start text-2xl font-medium"
-											onClick={(e) => e.stopPropagation()} // Prevent closing dropdown
-										>
-											{product.title}
-										</motion.button>
-									))}
-								</motion.div>
-							) : null}
+							{(activeProducts?.length ?? 0) > 0 && (
+								<DropdownColumn className="z-30" key="productsPanel">
+									<DropdownLabel key={activeSubCategory?.slug}>
+										{activeSubCategory?.title ?? 'Danh mục sản phẩm'}
+									</DropdownLabel>
+									{activeProducts?.map((product, index) => {
+										const icon =
+											product.icon && typeof product.icon === 'object'
+												? product.icon
+												: null
+
+										return (
+											<motion.button
+												key={product.slug}
+												custom={index}
+												variants={itemAnimationVariants}
+												initial="initial"
+												animate="animate"
+												exit="exit"
+												onClick={(e) => e.stopPropagation()} // Prevent closing dropdown
+											>
+												<Link
+													href={`/product/${product.slug}`}
+													tabIndex={-1}
+													className="group grid gap-x-4"
+													style={{
+														gridTemplateAreas: `"title image"
+																			"description image"
+																			"price image"`,
+													}}
+												>
+													<div
+														className="group-hover:hover-underline-animation group-hover:keep-hover w-fit text-balance text-start text-lg font-medium"
+														style={{ gridArea: 'title' }}
+													>
+														{product.title}
+													</div>
+													<div
+														className="my-1 text-balance text-start text-base font-medium opacity-70"
+														style={{ gridArea: 'description' }}
+													>
+														{product.shortDescription}
+													</div>
+													<div
+														className="text-start text-base font-medium opacity-70"
+														style={{ gridArea: 'price' }}
+													>
+														{formatPrice(product.price)}
+													</div>
+													<Image
+														src={icon?.url ?? 'https://placehold.co/200x200'}
+														alt={icon?.alt ?? 'Product Icon'}
+														width={icon?.width ?? 200}
+														height={icon?.height ?? 200}
+														unoptimized={icon === null}
+														className="size-[5.25rem] object-contain"
+														style={{ gridArea: 'image' }}
+													/>
+												</Link>
+											</motion.button>
+										)
+									})}
+								</DropdownColumn>
+							)}
 						</AnimatePresence>
 					</motion.div>
 				)}
