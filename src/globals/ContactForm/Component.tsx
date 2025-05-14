e567fea1843dab54037e5e8d8737a15824de601d
Delnegend <kiennguyen19323@gmail.com>
2025-05-14 16:58:38 +0700

    feat(`globals/ContactForm`): rename component; validate w/ zod; save to db

--- src/globals/ContactForm/Component.tsx
+++ src/globals/ContactForm/Component.tsx
@@ -1,20 +1,20 @@
-import { DataFromGlobalSlug } from 'payload'
-
 import { DialogTitle } from '@/components/ui/dialog'
+import { ContactFormGlobal } from '@/payload-types'
 import { getCachedGlobal } from '@/utilities/getGlobals'
 
-import { ContactFormCC } from './Component.client'
+import { ContactFormClient } from './Component.client'
+import { ContactFormGlobalSlug } from './config'
 
 export async function ContactForm({ inDialog }: { inDialog?: boolean }) {
-	const data = (await getCachedGlobal('contact-form', 1)()) as DataFromGlobalSlug<'contact-form'>
+	const data = (await getCachedGlobal(ContactFormGlobalSlug, 1)()) as ContactFormGlobal
 
 	if (inDialog) {
 		return (
 			<>
 				<DialogTitle className="sr-only">{data.title}</DialogTitle>
-				<ContactFormCC data={data} />
+				<ContactFormClient data={data} />
 			</>
 		)
 	}
-	return <ContactFormCC data={data} />
+	return <ContactFormClient data={data} />
 }
