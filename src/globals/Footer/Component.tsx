97c4a703045d24071d6853c673f45a97c82b165d
Delnegend <kiennguyen19323@gmail.com>
2025-05-15 18:23:03 +0700

    feat(`globals/Footer/Component`): rename size name; complete footer small & minimum

--- src/globals/Footer/Component.tsx
+++ src/globals/Footer/Component.tsx
@@ -2,7 +2,7 @@ import { Lato, Phudu } from 'next/font/google'
 import Image from 'next/image'
 
 import { TextInput } from '@/components/ui/text-input'
-import { FooterGlobal } from '@/payload-types'
+import { FooterGlobal, Media } from '@/payload-types'
 import { getCachedGlobal } from '@/utilities/getGlobals'
 import { cn } from '@/utilities/ui'
 
@@ -11,38 +11,46 @@ import { FooterGlobalSlug } from './config'
 const lato = Lato({ subsets: ['latin'], weight: ['400'] })
 const phudu = Phudu({ subsets: ['vietnamese'], weight: ['400', '600', '700'] })
 
-export async function FooterComponent({ size }: { size?: 'sm' | 'lg' | 'md' }) {
-	const globals = (await getCachedGlobal(FooterGlobalSlug, 1)()) as FooterGlobal
-
-	switch (size) {
-		case 'lg':
-			return <FooterLarge globals={globals} />
-		case 'md':
-			return <FooterMedium globals={globals} />
-		default:
-			return <FooterSmall globals={globals} />
-	}
-}
-async function FooterLarge({ globals }: { globals: FooterGlobal }): Promise<React.JSX.Element> {
-	const { contactUs, legal, image: img } = globals
-
-	const image =
-		img !== undefined && typeof img.image === 'object' && img.image !== null ? img.image : null
+export async function FooterComponent({ size }: { size?: 'small' | 'large' | 'medium' | null }) {
+	const global = (await getCachedGlobal(FooterGlobalSlug, 1)()) as FooterGlobal
 
 	const stamp =
-		legal.stamp !== undefined && typeof legal.stamp === 'object' && legal.stamp !== null
-			? legal.stamp
-			: undefined
+		global.legal.stamp !== undefined &&
+		typeof global.legal.stamp === 'object' &&
+		global.legal.stamp !== null
+			? global.legal.stamp
+			: null
+
+	switch (size) {
+		case 'large':
+			return <FooterLarge global={global} stamp={stamp} />
+		case 'medium':
+			return <FooterMedium global={global} stamp={stamp} />
+		default:
+			return <FooterSmall global={global} />
+	}
+}
+async function FooterLarge({
+	global: { contactUs, legal, image },
+	stamp,
+}: {
+	global: FooterGlobal
+	stamp?: Media | null
+}): Promise<React.JSX.Element> {
+	const img =
+		image !== undefined && typeof image.image === 'object' && image.image !== null
+			? image.image
+			: null
 
 	return (
 		<footer className="relative flex overflow-hidden">
 			<Image
-				src={image?.url ?? 'https://placehold.co/1000x1000'}
-				alt={image?.alt ?? 'Lanscape'}
-				width={image?.width ?? 1000}
-				height={image?.height ?? 1000}
+				src={img?.url ?? 'https://placehold.co/1000x1000'}
+				alt={img?.alt ?? 'Lanscape'}
+				width={img?.width ?? 1000}
+				height={img?.height ?? 1000}
 				className="absolute top-1/2 h-full w-1/2 -translate-y-1/2 object-cover"
-				unoptimized={image === null}
+				unoptimized={!img}
 			/>
 
 			<div className="w-1/2 translate-x-full">
@@ -71,8 +79,8 @@ async function FooterLarge({ globals }: { globals: FooterGlobal }): Promise<Reac
 						alt={stamp?.alt ?? 'Đã thông báo bộ Công Thương'}
 						width={stamp?.width ?? 200}
 						height={stamp?.height ?? 100}
-						className="my-6 max-h-14 w-fit object-contain"
-						unoptimized={stamp?.url === undefined}
+						className="my-6 h-14 w-auto object-contain"
+						unoptimized={!stamp}
 					/>
 
 					<div className="text-xs font-normal text-[#F1DAAE]">{legal.copyright}</div>
@@ -82,10 +90,47 @@ async function FooterLarge({ globals }: { globals: FooterGlobal }): Promise<Reac
 	)
 }
 
-async function FooterMedium({ globals }: { globals: FooterGlobal }): Promise<React.JSX.Element> {
-	return <></>
+async function FooterMedium({
+	global,
+	stamp,
+}: {
+	global: FooterGlobal
+	stamp?: Media | null
+}): Promise<React.JSX.Element> {
+	return (
+		<div
+			className={cn(
+				'grid grid-cols-2 bg-[#210E0A] px-24 py-16 text-[hsl(39,71%,81%)]',
+				phudu.className,
+			)}
+		>
+			<div>
+				<div className="text-2xl font-bold">{global.legal.title}</div>
+				<div className="whitespace-pre-wrap text-base">{global.legal.content}</div>
+				<Image
+					src={stamp?.url ?? 'https://placehold.co/200x100'}
+					alt={stamp?.alt ?? 'Đã thông báo bộ Công Thương'}
+					width={stamp?.width ?? 200}
+					height={stamp?.height ?? 100}
+					className="my-6 h-14 w-auto object-contain"
+					unoptimized={!stamp}
+				/>
+			</div>
+
+			<div>
+				<div className="text-2xl font-bold">{global.legal.title}</div>
+				<div className="whitespace-pre-wrap text-base">{global.legal.content}</div>
+			</div>
+
+			<div className="col-span-2">{global.legal.copyright}</div>
+		</div>
+	)
 }
 
-async function FooterSmall({ globals }: { globals: FooterGlobal }): Promise<React.JSX.Element> {
-	return <></>
+async function FooterSmall({ global }: { global: FooterGlobal }): Promise<React.JSX.Element> {
+	return (
+		<div className="bg-[#210E0A] py-11 text-center text-xs text-[#F1DAAE]">
+			{global.legal.copyright}
+		</div>
+	)
 }
