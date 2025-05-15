fcf6774ef0cc9644aaa36a7dddb986d09b8435a8
Delnegend <kiennguyen19323@gmail.com>
2025-05-15 18:09:12 +0700

    feat(`blocks/PostsGrid`): auto-populate posts from post category

--- src/blocks/PostsGrid/Component.tsx
+++ src/blocks/PostsGrid/Component.tsx
@@ -1,12 +1,25 @@
+import config from '@payload-config'
+import { getPayload } from 'payload'
+
+import { PostCategoriesSlug } from '@/collections/PostCategories/slug'
+import { PostsSlug } from '@/collections/Posts/slug'
 import { PostCard } from '@/components/PostCard'
 import { PostsGridBlockProps } from '@/payload-types'
 
-export function PostsGridBlock(props: PostsGridBlockProps): React.JSX.Element {
-	const posts = props.posts?.filter((post) => typeof post === 'object') ?? []
+export async function PostsGridBlock(props: PostsGridBlockProps): Promise<React.JSX.Element> {
+	const payload = await getPayload({ config })
+	const posts = await payload.find({
+		collection: PostsSlug,
+		where: {
+			[`${PostCategoriesSlug}.slug`]: {
+				equals: typeof props.postCategories === 'object' ? props.postCategories?.slug : '',
+			},
+		},
+	})
 
 	return (
 		<div className="safe-width my-20 flex flex-row flex-wrap justify-center gap-x-10">
-			{posts.map((p) => {
+			{posts.docs.map((p) => {
 				return <PostCard key={p.slug} post={p} />
 			})}
 		</div>
