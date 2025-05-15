723c837b722ee5864346b62317f517fce02e2bbf
Delnegend <kiennguyen19323@gmail.com>
2025-05-15 18:11:24 +0700

    refactor(`components/RichText`): replace `posts` w/ `post`

--- src/components/RichText/index.tsx
+++ src/components/RichText/index.tsx
@@ -33,7 +33,7 @@ const internalDocToHref = ({ linkNode }: { linkNode: SerializedLinkNode }) => {
 		throw new Error('Expected value to be an object')
 	}
 	const slug = value.slug
-	return relationTo === 'posts' ? `/posts/${slug}` : `/${slug}`
+	return relationTo === 'posts' ? `/post/${slug}` : `/${slug}`
 }
 
 const jsxConverters: JSXConvertersFunction<NodeTypes> = ({ defaultConverters }) => ({
