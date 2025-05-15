541890bc06787c8959f588f31b4d4391fbdc7438
Delnegend <kiennguyen19323@gmail.com>
2025-05-15 18:22:23 +0700

    feat(`globals/ContactForm/Component.client`): handle errors

--- src/globals/ContactForm/Component.client.tsx
+++ src/globals/ContactForm/Component.client.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import Link from 'next/link'
-import { useForm } from 'react-hook-form'
+import { type SubmitHandler, useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { ZodError } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
@@ -10,43 +12,63 @@ import { ContactFormGlobal } from '@/payload-types'
 
 import { type ContactFormInputType, submitContactForm } from './actions/submitContactForm'
 
-export function ContactFormClient({ data }: { data: ContactFormGlobal }) {
+export function ContactFormClient({ global }: { global: ContactFormGlobal }) {
 	const { register, handleSubmit } = useForm<ContactFormInputType>()
+	const onSubmit: SubmitHandler<ContactFormInputType> = (data) => {
+		void (async (): Promise<void> => {
+			const response = await submitContactForm(data)
+			if (response.success) {
+				toast.success('BioLAK đã nhận đuợc câu hỏi của bạn!')
+				return
+			}
+			let description = ''
+			if (response.errors instanceof ZodError) {
+				for (const error of response.errors.errors) {
+					description += `${error.path.join('.')} ${error.message}\n`
+				}
+			} else {
+				description = `${response.errors}`
+			}
+			toast.error('Không thể gửi biểu mẫu!', {
+				description,
+			})
+		})()
+	}
 
 	return (
-		<Card className="flex flex-col gap-[4.5rem] border-none bg-transparent">
-			<CardHeader className="p-0">
-				<CardTitle className="h-[70px] font-serif text-4xl">{data.title}</CardTitle>
-			</CardHeader>
-			<CardContent className="p-0">
-				<form onSubmit={handleSubmit(submitContactForm)}>
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<Card className="flex flex-col gap-[4.5rem] border-none bg-transparent">
+				<CardHeader className="p-0">
+					<CardTitle className="h-[70px] font-serif text-4xl">{global.title}</CardTitle>
+				</CardHeader>
+				<CardContent className="p-0">
 					<div className="grid grid-cols-2 gap-9">
-						<TextInput label={data.name} {...register('username')} />
-						<TextInput label={data.phoneNumber} {...register('phoneNumber')} />
+						<TextInput label={global.name} {...register('username')} />
+						<TextInput label={global.phoneNumber} {...register('phoneNumber')} />
 						<TextInput
 							classNames={{ container: 'col-span-2' }}
-							label={data.email}
+							label={global.email}
 							type="email"
 							{...register('email')}
 						/>
 						<TextInput
 							classNames={{ container: 'col-span-2' }}
-							label={data.question}
+							label={global.question}
 							{...register('message')}
 						/>
 					</div>
-				</form>
-			</CardContent>
-			<CardFooter className="grid grid-cols-2 gap-5 p-0">
-				<Button onClick={handleSubmit(submitContactForm)} size="lg">
-					{data.actionSend}
-				</Button>
-				<Link href={`tel:${data.biolakPhoneNumber}`} tabIndex={-1}>
-					<Button className="pointer-events-none w-full" variant="outline" size="lg">
-						{data.actionCall}
+				</CardContent>
+				<CardFooter className="grid grid-cols-2 gap-5 p-0">
+					<Button size="lg" type="submit">
+						{global.actionSend}
 					</Button>
-				</Link>
-			</CardFooter>
-		</Card>
+					<Link href={`tel:${global.biolakPhoneNumber}`} tabIndex={-1}>
+						<Button className="pointer-events-none w-full" variant="outline" size="lg">
+							{global.actionCall}
+						</Button>
+					</Link>
+				</CardFooter>
+			</Card>
+		</form>
 	)
 }
