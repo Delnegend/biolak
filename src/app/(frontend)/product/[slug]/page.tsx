a22882ed30149d499c4fb67697582ae89bf93180
Delnegend <kiennguyen19323@gmail.com>
2025-05-19 01:48:34 +0700

    refactor(`app/fe/product/slug/page`): disable `overrideAccess`

--- src/app/(frontend)/product/[slug]/page.tsx
+++ src/app/(frontend)/product/[slug]/page.tsx
@@ -40,7 +40,7 @@ const queryProductBySlug = cache(async ({ slug }: { slug: string }) => {
 		collection: ProductsSlug,
 		draft,
 		limit: 1,
-		overrideAccess: draft,
+		overrideAccess: false,
 		pagination: false,
 		where: {
 			slug: {
