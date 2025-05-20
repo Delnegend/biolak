3fb442e6a6ed18b2516d3b22dc89f1d3ebe453e3
Delnegend <kiennguyen19323@gmail.com>
2025-05-20 11:05:50 +0700

    feat(`blocks`): WIP `HowToUseProduct`

---
+++ src/blocks/HowToUseProduct/Component.tsx
@@ -0,0 +1,7 @@
+import { HowToUseProductBlockProps } from '@/payload-types'
+
+export function HowToUseProductBlockComponent(
+	_props: HowToUseProductBlockProps,
+): React.JSX.Element {
+	return <div className="grid grid-cols-2"></div>
+}
