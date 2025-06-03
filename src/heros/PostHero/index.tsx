d190b6125a90bf24944c921476f74d5c0e86d46d
Delnegend <kiennguyen19323@gmail.com>
2025-06-03 13:26:29 +0700

    feat(heros/PostHero): i18n

--- src/heros/PostHero/index.tsx
+++ src/heros/PostHero/index.tsx
@@ -2,6 +2,9 @@ import { Phudu } from 'next/font/google'
 import React from 'react'
 
 import type { Post } from '@/payload-types'
+import { getClientLang } from '@/utilities/getClientLang'
+import { Lang } from '@/utilities/lang'
+import { matchLang } from '@/utilities/matchLang'
 import { cn } from '@/utilities/ui'
 
 const phudu = Phudu({
@@ -9,7 +12,8 @@ const phudu = Phudu({
 	weight: ['600'],
 })
 
-export function PostHero({ post: p }: { post: Post }): React.JSX.Element {
+export async function PostHero({ post: p }: { post: Post }): Promise<React.JSX.Element> {
+	const locale = await getClientLang()
 	// const hasAuthors =
 	// 	p.populatedAuthors &&
 	// 	p.populatedAuthors.length > 0 &&
@@ -17,7 +21,12 @@ export function PostHero({ post: p }: { post: Post }): React.JSX.Element {
 
 	return (
 		<div className="safe-width mt-14 text-primary">
-			<div className={cn('mb-7 text-2xl font-medium', phudu.className)}>BÀI VIẾT</div>
+			<div className={cn('mb-7 text-2xl font-medium uppercase', phudu.className)}>
+				{matchLang({
+					[Lang.English]: 'Post',
+					[Lang.Vietnamese]: 'Bài viết',
+				})({ locale })}
+			</div>
 			<h1 className="max-w-[30rem] text-balance">{p.title}</h1>
 		</div>
 	)
