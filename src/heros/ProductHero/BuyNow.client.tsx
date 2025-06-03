fc23af7004d0d1bea5b9fd273c49838d1ea7e452
Delnegend <kiennguyen19323@gmail.com>
2025-06-03 13:35:54 +0700

    feat(heros/ProductHero): functional add to cart & buy now btns

---
+++ src/heros/ProductHero/BuyNow.client.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { useClientLang } from '@/hooks/useClientLang'
+import { BasicProduct } from '@/utilities/cartManager'
+import { formatPrice } from '@/utilities/formatPrice'
+import { Lang } from '@/utilities/lang'
+import { matchLang } from '@/utilities/matchLang'
+
+export function INTERNAL_BuyNowClient({ product }: { product: BasicProduct }): React.JSX.Element {
+	const { lang: locale } = useClientLang()
+
+	return (
+		<Button
+			size="lg"
+			className="flex items-center justify-between gap-4 uppercase"
+			hideArrow={true}
+			tabIndex={-1}
+		>
+			<a href={'/checkout?product=' + product.slug}>
+				{matchLang({
+					[Lang.English]: 'BUY NOW',
+					[Lang.Vietnamese]: 'MUA NGAY',
+				})({ locale })}{' '}
+				- {formatPrice(product.price)}
+			</a>
+		</Button>
+	)
+}
