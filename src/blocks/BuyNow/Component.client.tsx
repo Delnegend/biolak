42436af433fb7189d4ef6a33bc6b99e1287bdc89
Delnegend <kiennguyen19323@gmail.com>
2025-06-03 19:47:44 +0700

    feat(`blocks/BuyNow`): handle product sku

--- src/blocks/BuyNow/Component.client.tsx
+++ src/blocks/BuyNow/Component.client.tsx
@@ -1,18 +1,32 @@
 import { Button } from '@/components/ui/button'
+import { useSelectProductVariantUnsafe } from '@/heros/ProductHero/ProductVariantContext'
 
 export function INTERNAL_BuyNowClient(props: {
 	productSlug?: string | null
+	fallbackVariantSku?: string | null
 	buttonLabel: string
 }): React.JSX.Element {
+	const tmp = useSelectProductVariantUnsafe()
+	const variantSku = tmp?.product.variant?.sku
+
 	return (
 		<Button
 			size="lg"
 			variant="outline"
 			className="mb-[6rem] w-full max-w-[47rem] border-primary text-primary"
 			tabIndex={-1}
-			disabled={!props.productSlug}
+			disabled={!props.productSlug || !variantSku}
 		>
-			<a href={'/checkout?product=' + props.productSlug}>{props.buttonLabel}</a>
+			<a
+				href={
+					'/checkout?product=' +
+					props.productSlug +
+					'&variant=' +
+					(variantSku ?? props.fallbackVariantSku)
+				}
+			>
+				{props.buttonLabel}
+			</a>
 		</Button>
 	)
 }
