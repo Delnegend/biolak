931a7e14be4fa751f0f4727b1da281335945fb0b
Delnegend <kiennguyen19323@gmail.com>
2025-06-04 12:10:54 +0700

    refactor(`heros/ProductHero/BuyNow`): rename variables; fallback variant; medium button

--- src/heros/ProductHero/BuyNow.client.tsx
+++ src/heros/ProductHero/BuyNow.client.tsx
@@ -2,41 +2,48 @@
 
 import { Button } from '@/components/ui/button'
 import { useClientLang } from '@/hooks/useClientLang'
+import { Product } from '@/payload-types'
 import { formatPrice } from '@/utilities/formatPrice'
 import { Lang } from '@/utilities/lang'
 import { matchLang } from '@/utilities/matchLang'
 
 import { useSelectProductVariant } from './ProductVariantContext'
 
-export function INTERNAL_BuyNowClient(): React.JSX.Element {
+export function INTERNAL_BuyNowClient({
+	fallbackVariant,
+}: {
+	fallbackVariant?: Product['variants'][number] | null
+}): React.JSX.Element {
 	const { lang: locale } = useClientLang()
-	const { product } = useSelectProductVariant()
+	const { selectedProductVariant } = useSelectProductVariant()
+
+	const variant = selectedProductVariant?.variant ?? fallbackVariant ?? undefined
 
 	return (
 		<Button
-			size="lg"
+			size="md"
 			className="flex items-center justify-between gap-4 uppercase"
 			hideArrow={true}
 			tabIndex={-1}
-			disabled={!product.variant}
+			disabled={!variant}
 		>
 			<a
-				href={'/checkout?product=' + product.slug}
+				href={'/checkout?product=' + selectedProductVariant?.slug}
 				onClick={(e) => {
-					if (!product.variant) {
+					if (!variant) {
 						e.preventDefault()
 					}
 				}}
 				aria-label={matchLang({
-					[Lang.English]: `Buy ${product.title} variant ${product.variant?.title} now`,
-					[Lang.Vietnamese]: `Mua ngay ${product.title} loại ${product.variant?.title}`,
+					[Lang.English]: `Buy ${selectedProductVariant?.title} variant ${variant?.title} now`,
+					[Lang.Vietnamese]: `Mua ngay ${selectedProductVariant?.title} loại ${variant?.title}`,
 				})({ locale })}
 			>
 				{matchLang({
 					[Lang.English]: 'BUY NOW',
 					[Lang.Vietnamese]: 'MUA NGAY',
 				})({ locale })}
-				{product.variant && <span> - {formatPrice(product.variant.price)}</span>}
+				{variant && <span> - {formatPrice(variant.price)}</span>}
 			</a>
 		</Button>
 	)
