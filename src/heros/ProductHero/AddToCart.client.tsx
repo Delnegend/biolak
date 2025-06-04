14adaa848f289728a3e4abbd491fbbb7ad12896a
Delnegend <kiennguyen19323@gmail.com>
2025-06-04 12:10:34 +0700

    refactor(heros/ProductHero/AddToCart): rename vars; medium button

--- src/heros/ProductHero/AddToCart.client.tsx
+++ src/heros/ProductHero/AddToCart.client.tsx
@@ -14,35 +14,41 @@ import { useSelectProductVariant } from './ProductVariantContext'
 export function INTERNAL_AddToCartClient(): React.JSX.Element {
 	const { lang: locale } = useClientLang()
 	const { loadProduct } = useCartManager({ syncWithLocalStorage: true })
-	const { product } = useSelectProductVariant()
+	const { selectedProductVariant } = useSelectProductVariant()
 
 	return (
 		<Button
-			size="lg"
+			size="md"
 			className="flex items-center justify-between gap-4 border-black uppercase text-black"
 			hideArrow={true}
 			variant="outline"
 			onClick={() => {
-				if (!product.slug || !product.title || !product.variant) return
-				loadProduct(
-					{
-						slug: product.slug,
-						title: product.title,
-						variant: product.variant,
-					},
-					1,
+				if (
+					!selectedProductVariant?.slug ||
+					!selectedProductVariant?.title ||
+					!selectedProductVariant?.variant
 				)
+					return
+				loadProduct({
+					slug: selectedProductVariant?.slug,
+					title: selectedProductVariant?.title,
+					variant: selectedProductVariant?.variant,
+				})
 				toast.success(
 					matchLang({
-						[Lang.English]: `Added ${product.title} variant ${product.variant?.title} to cart`,
-						[Lang.Vietnamese]: `Đã thêm ${product.title} loại ${product.variant?.title} vào giỏ hàng`,
+						[Lang.English]: `Added ${selectedProductVariant?.title} variant ${selectedProductVariant?.variant?.title} to cart`,
+						[Lang.Vietnamese]: `Đã thêm ${selectedProductVariant?.title} loại ${selectedProductVariant?.variant?.title} vào giỏ hàng`,
 					})({ locale }),
 				)
 			}}
-			disabled={!product.slug || !product.title || !product.variant}
+			disabled={
+				!selectedProductVariant?.slug ||
+				!selectedProductVariant?.title ||
+				!selectedProductVariant?.variant
+			}
 			aria-label={matchLang({
-				[Lang.English]: `Add ${product.title} variant ${product.variant?.title} to cart`,
-				[Lang.Vietnamese]: `Thêm ${product.title} loại ${product.variant?.title} vào giỏ hàng`,
+				[Lang.English]: `Add ${selectedProductVariant?.title} variant ${selectedProductVariant?.variant?.title} to cart`,
+				[Lang.Vietnamese]: `Thêm ${selectedProductVariant?.title} loại ${selectedProductVariant?.variant?.title} vào giỏ hàng`,
 			})({ locale })}
 		>
 			<span>
