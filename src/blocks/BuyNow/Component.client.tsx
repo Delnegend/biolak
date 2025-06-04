d348f36165d6a99c086006b67f473a4672bb2390
Delnegend <kiennguyen19323@gmail.com>
2025-06-04 23:49:19 +0700

    fix(`blocks/BuyNow/Component.client`): rename fns, vars; `a` inside `button`; use fallback sku

--- src/blocks/BuyNow/Component.client.tsx
+++ src/blocks/BuyNow/Component.client.tsx
@@ -1,33 +1,31 @@
 'use client'
 
+import { ArrowRight } from 'lucide-react'
+
 import { Button } from '@/components/ui/button'
-import { useSelectProductVariantUnsafe } from '@/heros/ProductHero/ProductVariantContext'
+import { useNullableSelectedProductVariant } from '@/heros/ProductHero/ProductVariantContext'
 
 export function INTERNAL_BuyNowClient(props: {
 	productSlug?: string | null
 	fallbackVariantSku?: string | null
 	buttonLabel: string
 }): React.JSX.Element {
-	const tmp = useSelectProductVariantUnsafe()
-	const variantSku = tmp?.selectedProductVariant?.variant?.sku
+	const variantSku =
+		useNullableSelectedProductVariant()?.selectedProductVariant?.variant?.sku ??
+		props.fallbackVariantSku
 
 	return (
 		<Button
 			size="lg"
 			variant="outline"
-			className="w-full max-w-[47rem] border-primary text-primary"
+			className="w-full max-w-[47rem] justify-between border-primary text-primary"
 			tabIndex={-1}
 			disabled={!props.productSlug || !variantSku}
+			asChild
 		>
-			<a
-				href={
-					'/checkout?product=' +
-					props.productSlug +
-					'&variant=' +
-					(variantSku ?? props.fallbackVariantSku)
-				}
-			>
+			<a href={'/checkout?product=' + props.productSlug + '&variant=' + variantSku}>
 				{props.buttonLabel}
+				<ArrowRight />
 			</a>
 		</Button>
 	)
