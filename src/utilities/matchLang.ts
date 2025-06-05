66ac57d6ed07aaf2dd99c2f445a549512db6709d
Delnegend <kiennguyen19323@gmail.com>
2025-06-06 00:03:01 +0700

    refactor(`utils/matchLang`): flexible props

--- src/utilities/matchLang.ts
+++ src/utilities/matchLang.ts
@@ -2,8 +2,12 @@ import { defaultLocale, Lang } from './lang'
 
 export function matchLang(
 	translations: Record<Lang, string>,
-): ({ locale }: { locale: Lang }) => string {
-	return ({ locale }: { locale: Lang }): string => {
+): (props: { locale: Lang } | Lang) => string {
+	return function matchLangInner(props: { locale: Lang } | Lang): string {
+		if (typeof props === 'string') {
+			return translations[props as Lang] ?? translations[defaultLocale] ?? ''
+		}
+		const { locale } = props
 		const t = translations[locale]
 		if (t) return t
 		const fallback = translations[defaultLocale]
