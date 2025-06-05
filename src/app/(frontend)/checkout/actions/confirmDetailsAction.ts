21fec5d89280a8b537d1b4420d2c48da5a77b81e
Delnegend <kiennguyen19323@gmail.com>
2025-06-05 11:08:39 +0700

    feat(`app/fe/checkout`): complete ui, needs logic

---
+++ src/app/(frontend)/checkout/actions/confirmDetailsAction.ts
@@ -0,0 +1,91 @@
+import { z } from 'zod/v4'
+
+import { getClientLang } from '@/utilities/getClientLang'
+import { Lang } from '@/utilities/lang'
+import { matchLang } from '@/utilities/matchLang'
+import { tryCatchSync } from '@/utilities/tryCatch'
+
+import CITY_DISTRICT_WARD from './city-district-ward.json'
+
+interface CityDistrictWard {
+	[key: string]: {
+		[key: string]: string[]
+	}
+}
+
+const ConfirmDetailsActionSchema = z.object({
+	personalDetails: z
+		.object({
+			name: z.string().min(1, 'Name is required'),
+			email: z.email().min(1, 'Email is required'),
+			phoneNumber: z.string().min(1, 'Phone number is required'),
+			city: z.enum(Object.keys(CITY_DISTRICT_WARD)),
+			district: z.string().min(1, 'District is required'),
+			ward: z.string().min(1, 'Ward is required'),
+			houseNumber: z.string().min(1, 'House number is required'),
+		})
+		.required(),
+	transportationMethod: z.enum(['standard', 'express']),
+	paymentMethod: z.enum(['cod', 'bankTransfer']),
+	sendGift: z.object({
+		sender: z.string().optional(),
+		receiver: z.string().optional(),
+		message: z.string().max(1000).optional(),
+	}),
+})
+
+export type ConfirmDetailsAction = z.infer<typeof ConfirmDetailsActionSchema>
+
+export async function confirmDetailsAction(input: unknown): Promise<
+	| {
+			success: true
+	  }
+	| {
+			success: false
+			error: string
+	  }
+> {
+	const parsedInput = tryCatchSync(() => ConfirmDetailsActionSchema.safeParse(input))
+	if (!parsedInput.success) {
+		if (parsedInput.error instanceof z.ZodError) {
+			return {
+				success: false,
+				error: parsedInput.error.message,
+			}
+		}
+		return {
+			success: false,
+			error: `${parsedInput.error}`,
+		}
+	}
+
+	const locale = await getClientLang()
+	if (!parsedInput.data.data) {
+		return {
+			success: false,
+			error: matchLang({
+				[Lang.English]: 'Invalid input data',
+				[Lang.Vietnamese]: 'Dữ liệu đầu vào không hợp lệ',
+			})({ locale }),
+		}
+	}
+
+	const { personalDetails } = parsedInput.data.data
+	const { city, district, ward } = personalDetails
+	const cityDistrictWard: CityDistrictWard = CITY_DISTRICT_WARD as CityDistrictWard
+	if (
+		!cityDistrictWard[city] ||
+		!cityDistrictWard[city][district] ||
+		!cityDistrictWard[city][district].includes(ward)
+	) {
+		return {
+			success: false,
+			error: matchLang({
+				[Lang.English]: 'Invalid district or ward for the selected city',
+				[Lang.Vietnamese]: 'Quận hoặc phường không hợp lệ cho thành phố đã chọn',
+			})({ locale }),
+		}
+	}
+
+	return { success: true }
+}
--- src/app/(frontend)/checkout/page.client.tsx
+++ src/app/(frontend)/checkout/page.client.tsx
@@ -1,10 +1,17 @@
 'use client'
-import { CircleMinus, CirclePlus, X } from 'lucide-react'
-import Image from 'next/image'
-import React, { useEffect } from 'react'
+import { ArrowRight, ChevronDown } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
 
+import { CartListClient } from '@/components/CartList.client'
 import { Button } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox'
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select'
 import { TextInput } from '@/components/ui/text-input'
 import { BasicProductInCart, useCartManager } from '@/hooks/useCartManager'
 import { useClientLang } from '@/hooks/useClientLang'
@@ -14,20 +21,27 @@ import { Lang } from '@/utilities/lang'
 import { matchLang } from '@/utilities/matchLang'
 import { cn } from '@/utilities/ui'
 
+import CITY_DISTRICT_WARD from './actions/city-district-ward.json'
+
 function Title({
 	children,
 	className,
 	...props
 }: React.ComponentPropsWithRef<'h3'>): React.JSX.Element {
 	return (
-		<h3 className={cn('font-sans text-xl font-bold leading-8', className)} {...props}>
+		<h3
+			className={cn('font-sans text-xl font-bold leading-8 text-primary', className)}
+			{...props}
+		>
 			{children}
 		</h3>
 	)
 }
 
 function Card({ children }: { children: React.ReactNode }): React.JSX.Element {
-	return <div className="flex flex-col gap-y-9 bg-white p-6">{children}</div>
+	return (
+		<div className="flex flex-col gap-y-4 rounded-xl bg-white p-6 text-primary">{children}</div>
+	)
 }
 
 type BasicProductInCheckout = Omit<BasicProductInCart, 'checked'>
@@ -55,6 +69,75 @@ function CustomizedCheckbox({
 	)
 }
 
+function CartListWithAccordion(): React.JSX.Element {
+	const { lang: locale } = useClientLang()
+	const [open, setOpen] = useState(false)
+
+	return (
+		<div>
+			<div
+				className={cn('grid transition-all', {
+					'grid-rows-[1fr]': open,
+					'grid-rows-[0fr]': !open,
+				})}
+			>
+				<CartListClient showCheckbox={false} className="mb-4 min-h-0 overflow-hidden" />
+			</div>
+
+			<button
+				className="!flex w-full flex-row items-center justify-between font-sans text-xl"
+				aria-label={matchLang({
+					[Lang.English]: 'Toggle cart details',
+					[Lang.Vietnamese]: 'Ẩn hiện chi tiết giỏ hàng',
+				})({ locale })}
+				onClick={() => setOpen((prev) => !prev)}
+			>
+				{matchLang({
+					[Lang.English]: open ? 'Hide details' : 'Cart details',
+					[Lang.Vietnamese]: open ? 'Ẩn chi tiết' : 'Chi tiết giỏ hàng',
+				})({ locale })}
+				<ChevronDown
+					className={cn('transition-all', {
+						'rotate-180': open,
+						'rotate-0': !open,
+					})}
+				/>
+			</button>
+		</div>
+	)
+}
+
+function OrderSummary({
+	cart,
+	global,
+}: {
+	cart: BasicProductInCart[]
+	global: CheckoutPageGlobal
+}): React.JSX.Element {
+	const provisional = cart.reduce((acc, item) => acc + item.variant.price * item.quantity, 0)
+
+	return (
+		<div>
+			<div className="grid grid-cols-[auto_1fr] gap-2">
+				<div className="font-bold">{global.orderSummary.provisional}</div>
+				<div className="place-self-end">{formatPrice(provisional)}</div>
+				<div className="font-bold">{global.orderSummary.shipping}</div>
+				<div className="place-self-end">{formatPrice(0)}</div>
+				<div className="font-bold">{global.orderSummary.discount}</div>
+				<div className="place-self-end">{formatPrice(0)}</div>
+				<div className="font-bold">{global.orderSummary.total}</div>
+				<div className="place-self-end">{formatPrice(provisional)}</div>
+			</div>
+			<ul className="my-6 text-balance italic">
+				<li>{global.orderSummary.acknowledgment}</li>
+			</ul>
+			<Button size="md" className="h-14 w-full" hideArrow>
+				{global.orderSummary.orderButtonLabel}
+			</Button>
+		</div>
+	)
+}
+
 export default function PageClient({
 	global,
 	overrideProduct,
@@ -62,10 +145,31 @@ export default function PageClient({
 	global: CheckoutPageGlobal
 	overrideProduct?: Omit<BasicProductInCheckout, 'quantity'> | null
 }): React.JSX.Element {
-	const { cart, loadProduct, unloadProduct, removeProduct } = useCartManager({
+	const { cart, loadedFromLocalStorageDone, loadProduct } = useCartManager({
 		syncWithLocalStorage: !overrideProduct,
 	})
 	const { lang: locale } = useClientLang()
+
+	const [selectedCity, setSelectedCity_] = useState<string | undefined>(undefined)
+	function setSelectedCity(city: string | undefined): void {
+		setSelectedDistrict(undefined)
+		setSelectedCity_(city)
+		const inputElem = document.getElementById('provinceCity') as HTMLInputElement
+		if (inputElem) inputElem.value = city ?? ''
+	}
+	const [selectedDistrict, setSelectedDistrict_] = useState<string | undefined>(undefined)
+	function setSelectedDistrict(district: string | undefined): void {
+		setSelectedWard(undefined)
+		setSelectedDistrict_(district)
+		const inputElem = document.getElementById('district') as HTMLInputElement
+		if (inputElem) inputElem.value = district ?? ''
+	}
+	const [selectedWard, setSelectedWard_] = useState<string | undefined>(undefined)
+	function setSelectedWard(ward: string | undefined): void {
+		setSelectedWard_(ward)
+		const inputElem = document.getElementById('ward') as HTMLInputElement
+		if (inputElem) inputElem.value = ward ?? ''
+	}
 
 	useEffect(() => {
 		if (overrideProduct)
@@ -82,20 +186,27 @@ export default function PageClient({
 			<div className="flex flex-col gap-10">
 				<h2>
 					{matchLang({
-						[Lang.English]: 'Your cart is empty.',
-						[Lang.Vietnamese]: 'Giỏ hàng của bạn đang trống.',
+						[Lang.English]: loadedFromLocalStorageDone
+							? 'Your cart is empty.'
+							: 'Loading your cart...',
+						[Lang.Vietnamese]: loadedFromLocalStorageDone
+							? 'Giỏ hàng của bạn đang trống.'
+							: 'Đang tải giỏ hàng của bạn...',
 					})({ locale })}
 				</h2>
-				<Button tabIndex={-1} className="w-fit">
-					{/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-					<a href="/">
-						{matchLang({
-							[Lang.English]: 'Go to home page',
-							[Lang.Vietnamese]: 'Về trang chủ',
-						})({ locale })}
-						&nbsp;
-					</a>
-				</Button>
+				{loadedFromLocalStorageDone && (
+					<Button className="w-fit" asChild>
+						{/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+						<a href="/">
+							{matchLang({
+								[Lang.English]: 'Go to home page',
+								[Lang.Vietnamese]: 'Về trang chủ',
+							})({ locale })}
+							&nbsp;
+						</a>
+						<ArrowRight />
+					</Button>
+				)}
 			</div>
 		)
 	}
@@ -113,10 +224,73 @@ export default function PageClient({
 					<div className="grid grid-cols-2 gap-x-6 gap-y-9">
 						<TextInput size="sm" label={global.address.nameInputLabel} />
 						<TextInput size="sm" label={global.address.phoneInputLabel} />
-						<TextInput size="sm" label={global.address.provinceCityInputLabel} />
-						<TextInput size="sm" label={global.address.districtInputLabel} />
-						<TextInput size="sm" label={global.address.wardInputLabel} />
+						<input type="text" className="hidden" id="provinceCity" />
+						<input type="text" className="hidden" id="district" />
+						<input type="text" className="hidden" id="ward" />
+
+						<Select value={selectedCity} onValueChange={setSelectedCity}>
+							<SelectTrigger
+								label={global.address.provinceCityInputLabel}
+								aria-label={global.address.provinceCityInputLabel}
+							>
+								<SelectValue />
+							</SelectTrigger>
+							<SelectContent>
+								{Object.keys(CITY_DISTRICT_WARD).map((city) => (
+									<SelectItem key={city} value={city}>
+										{city}
+									</SelectItem>
+								))}
+							</SelectContent>
+						</Select>
+						<Select
+							value={selectedDistrict}
+							onValueChange={setSelectedDistrict}
+							disabled={!Boolean(selectedCity)}
+						>
+							<SelectTrigger
+								label={global.address.districtInputLabel}
+								aria-label={global.address.districtInputLabel}
+							>
+								<SelectValue />
+							</SelectTrigger>
+							<SelectContent>
+								{/* @ts-expect-error - idc */}
+								{Object.keys(selectedCity ? CITY_DISTRICT_WARD[selectedCity] : {}).map(
+									(district) => (
+										<SelectItem key={district} value={district}>
+											{district}
+										</SelectItem>
+									),
+								)}
+							</SelectContent>
+						</Select>
+						<Select
+							value={selectedWard}
+							onValueChange={setSelectedWard}
+							disabled={!Boolean(selectedDistrict) || !Boolean(selectedCity)}
+						>
+							<SelectTrigger
+								label={global.address.wardInputLabel}
+								aria-label={global.address.wardInputLabel}
+							>
+								<SelectValue />
+							</SelectTrigger>
+							<SelectContent>
+								{(selectedCity && selectedDistrict
+									? // @ts-expect-error - idc
+										CITY_DISTRICT_WARD[selectedCity][selectedDistrict]
+									: []
+								).map((ward: string) => (
+									<SelectItem key={ward} value={ward}>
+										{ward}
+									</SelectItem>
+								))}
+							</SelectContent>
+						</Select>
+
 						<TextInput size="sm" label={global.address.details} />
+
 						<CustomizedCheckbox
 							id="saveForNextTime"
 							label={global.address.saveForNextTime}
@@ -162,71 +336,29 @@ export default function PageClient({
 
 			<Card>
 				<Title>{global.order.title}</Title>
-				<div>
-					{cart.map((p) => {
-						return (
-							<div
-								key={p.slug}
-								className="grid grid-cols-[4rem_1fr_1fr_1fr_1f]"
-								style={{
-									gridTemplateAreas: `"icon title title title remove"
-																				"icon reduce count add price"`,
-								}}
-							>
-								<Image
-									src="https://placehold.co/200x200"
-									alt={matchLang({
-										[Lang.English]: 'Product icon of ' + p.title,
-										[Lang.Vietnamese]: 'Biểu tượng sản phẩm ' + p.title,
-									})({ locale })}
-									width={200}
-									height={200}
-									unoptimized={true}
-									className="h-full w-8 object-contain"
-									style={{ gridArea: 'icon' }}
-								/>
-								<Title style={{ gridArea: 'title' }}>{p.title}</Title>
-								<button
-									aria-label={matchLang({
-										[Lang.English]: `Remove ${p.title} from cart`,
-										[Lang.Vietnamese]: `Xoá ${p.title} khỏi giỏ hàng`,
-									})({ locale })}
-									onClick={() => removeProduct(p.slug)}
-								>
-									<X style={{ gridArea: 'remove' }} className="justify-self-end" />
-								</button>
+				<CartListWithAccordion />
+				<hr />
 
-								<button
-									aria-label={matchLang({
-										[Lang.English]: `Reduce ${p.title} amount to ${p.quantity - 1}`,
-										[Lang.Vietnamese]: `Giảm số lượng ${p.title} còn ${p.quantity - 1}`,
-									})({ locale })}
-									onClick={() => {
-										if (p.quantity > 1) unloadProduct(p, 1)
-									}}
-									disabled={p.quantity <= 1}
-								>
-									<CircleMinus style={{ gridArea: 'reduce' }} />
-								</button>
-
-								<button
-									aria-label={matchLang({
-										[Lang.English]: `Increase ${p.title} amount to ${p.quantity + 1}`,
-										[Lang.Vietnamese]: `Tăng số lượng ${p.title} lên ${p.quantity + 1}`,
-									})({ locale })}
-									onClick={() => loadProduct(p, 1)}
-								>
-									<CirclePlus style={{ gridArea: 'add' }} />
-								</button>
-
-								<div style={{ gridArea: 'count' }}>{p.quantity}</div>
-								<div style={{ gridArea: 'price' }} className="justify-self-end">
-									{formatPrice(p.variant.price)}
-								</div>
-							</div>
-						)
-					})}
+				<Title>{global.discount.title}</Title>
+				<div className="flex h-[4.5rem] items-center justify-between rounded-[0.5rem] border border-primary p-[0.625rem]">
+					<input
+						type="text"
+						placeholder={global.discount.inputPlaceholder}
+						aria-label={global.discount.inputPlaceholder}
+						className="h-[calc(4.5rem-1.25rem)] w-full border-primary bg-transparent px-3 text-lg placeholder:text-muted-foreground focus:outline-none"
+					/>
+					<Button
+						hideArrow
+						aria-label={global.discount.applyButtonLabel}
+						className="h-[calc(4.5rem-1.25rem)]"
+						size="md"
+					>
+						{global.discount.applyButtonLabel}
+					</Button>
 				</div>
+				<hr className="my-2" />
+
+				<OrderSummary cart={cart} global={global} />
 			</Card>
 		</div>
 	)
