61e91931af5b62f2ff5ff13ff23acbd5f5cf9e42
Delnegend <kiennguyen19323@gmail.com>
2025-06-15 16:19:43 +0700

    refactor(`blocks/CallToAddToCart`): cleaner; simplify props; passdown locale

--- src/blocks/CallToAddToCart/Component.tsx
+++ src/blocks/CallToAddToCart/Component.tsx
@@ -1,4 +1,3 @@
-import { ProductsSlug } from '@/collections/Products/slug'
 import { HeadlessImage } from '@/components/Media/HeadlessImage'
 import RichText from '@/components/RichText'
 import { CallToAddToCartBlockProps, Product } from '@/payload-types'
@@ -13,22 +12,6 @@ export function CallToAddToCartBlock(
 		__product?: Product | null
 		__locale?: Lang
 	},
-): Promise<React.JSX.Element> {
-	const locale = await getClientLang()
-	const p =
-		typeof props[ProductsSlug] === 'object' && !!props[ProductsSlug]
-			? props[ProductsSlug]
-			: props?.__product
-	if (!p) {
-		throw new Error(
-			'CallToAddToCartBlock must be used within a product page or specified with a product prop',
-		)
-	}
-
-	const variant =
-		p.variants?.find((v) => v.defaultVariant && v.stock > 0) ??
-		p.variants.find((v) => v.stock > 0)
-
 ): React.JSX.Element {
 	return (
 		<div className="safe-width my-28 flex !max-w-[50rem] flex-col items-center text-primary">
@@ -37,7 +20,6 @@ export function CallToAddToCartBlock(
 				alt={matchLang({
 					[Lang.English]: 'Background image for call to add to cart button',
 					[Lang.Vietnamese]: 'Hình nền cho nút thêm vào giỏ hàng',
-				})(locale)}
 				})(props.__locale)}
 				placeholder={{ width: 600, height: 600 }}
 				className="aspect-square size-full max-w-[38rem] rounded-full object-cover"
@@ -46,12 +28,6 @@ export function CallToAddToCartBlock(
 				<RichText data={props.content} enableGutter={false} className="mt-4 [&_li]:text-xl" />
 			)}
 			<INTERNAL_AddToCartClient
-				buttonLabel={props.buttonLabel ?? defaults.buttonLabel(locale)}
-				product={{
-					slug: p.slug,
-					title: p.title,
-					variant,
-				}}
 				buttonLabel={props.buttonLabel ?? defaults.buttonLabel(props.__locale)}
 			/>
 		</div>
