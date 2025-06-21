c73e918f9206d95f8dd5fdd89bd8cf2692dbda39
Delnegend <kiennguyen19323@gmail.com>
2025-06-21 18:30:42 +0700

    feat(utils): `depthHandler`

---
+++ src/utilities/depthHandler.ts
@@ -0,0 +1,48 @@
+import { tryCatch } from './tryCatch'
+
+/**
+ * Pretty wrapper for handling referenced data from Payload.
+ *
+ * Learn more: [Why does Payload use numbers in relations?](https://payloadcms.com/community-help/discord/why-does-payload-generated-types-have-numbers-as-a-possibility-in-so-many-relations)
+ */
+export async function depthHandler<T extends object>({
+	data,
+	fetch,
+}: {
+	data?: T | number | null
+	fetch: (id: number) => Promise<T>
+}): ReturnType<typeof tryCatch<T | null>> {
+	if (!data)
+		return {
+			data: null,
+			ok: true,
+			error: null,
+		}
+
+	if (typeof data === 'object')
+		return {
+			data,
+			ok: true,
+			error: null,
+		}
+
+	return tryCatch(async () => fetch(data))
+}
+
+export async function arrayDepthHandler<T extends object>({
+	data,
+	fetch,
+}: {
+	data?: (T | number)[]
+	fetch: (ids: number[]) => Promise<T[]>
+}): ReturnType<typeof tryCatch<T[]>> {
+	if (!data || typeof data[0] === 'object') {
+		return {
+			data: [],
+			ok: true,
+			error: null,
+		}
+	}
+
+	return tryCatch(async () => fetch(data.filter((item) => typeof item === 'number')))
+}
