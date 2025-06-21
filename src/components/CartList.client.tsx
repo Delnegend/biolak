eef2287fc3d441c1c039dec8eaeede8d57995cd6
Delnegend <kiennguyen19323@gmail.com>
2025-06-21 19:09:12 +0700

    refactor(`components/CartList`): rename import; inline component

--- src/components/CartList.client.tsx
+++ src/components/CartList.client.tsx
@@ -4,7 +4,7 @@ import { X } from 'lucide-react'
 import { Phudu } from 'next/font/google'
 import Link from 'next/link'
 
-import { useCartManager } from '@/hooks/useCartManager'
+import { ProductInCart, useCartManager } from '@/hooks/useCartManager'
 import { formatPrice } from '@/utilities/formatPrice'
 import { Lang } from '@/utilities/lang'
 import { matchLang } from '@/utilities/matchLang'
@@ -18,6 +18,120 @@ const phudu = Phudu({
 	weight: ['400'],
 })
 
+function CartItem({
+	productInCart,
+	showCheckbox,
+	locale,
+}: {
+	productInCart: ProductInCart
+	showCheckbox: boolean
+	locale?: Lang
+}): React.JSX.Element {
+	const { removeProduct, toggleCheck, loadProduct, unloadProduct } = useCartManager({
+		syncWithLocalStorage: true,
+	})
+
+	return (
+		<div
+			className={cn('grid items-center gap-3', {
+				'grid-cols-[auto_auto_1fr_auto]': showCheckbox,
+				'grid-cols-[auto_1fr_auto]': !showCheckbox,
+			})}
+			style={{
+				gridTemplateAreas: `"${showCheckbox ? 'select ' : ''}img title remove"
+														"${showCheckbox ? 'select ' : ''}img quantity price"`,
+			}}
+		>
+			{showCheckbox && (
+				<Checkbox
+					style={{ gridArea: 'select' }}
+					className="place-self-center"
+					aria-checked={productInCart.checked}
+					checked={productInCart.checked}
+					onCheckedChange={(checked) =>
+						toggleCheck({
+							productId: productInCart.product.id,
+							variantSku: productInCart.variant.sku,
+							checked: !!checked,
+						})
+					}
+				/>
+			)}
+			<Link href={`/product/${productInCart.product.slug}`} style={{ gridArea: 'img' }}>
+				<HeadlessImage
+					media={productInCart.variant.image}
+					alt={matchLang({
+						[Lang.English]: `${productInCart.product.title}'s image`,
+						[Lang.Vietnamese]: `Ảnh ${productInCart.product.title}`,
+					})(locale)}
+					placeholder={{ width: 100, height: 100 }}
+					className="size-[3.75rem] rounded-lg object-cover"
+				/>
+			</Link>
+			<Link
+				href={`/product/${productInCart.product.slug}`}
+				style={{ gridArea: 'title' }}
+				className="text-lg font-semibold text-primary"
+			>
+				{productInCart.product.title}
+			</Link>
+			<button
+				style={{ gridArea: 'remove' }}
+				className="place-self-end self-start text-primary"
+				onClick={() =>
+					removeProduct({
+						productId: productInCart.product.id,
+						variantSku: productInCart.variant.sku,
+					})
+				}
+				aria-label={matchLang({
+					[Lang.English]: `Remove ${productInCart.product.title} (${productInCart.variant.title}) from cart`,
+					[Lang.Vietnamese]: `Xoá ${productInCart.product.title} (${productInCart.variant.title}) khỏi giỏ hàng`,
+				})(locale)}
+			>
+				<X />
+			</button>
+			<div
+				style={{ gridArea: 'quantity' }}
+				className="flex h-5 w-[6.6rem] items-center justify-between border"
+			>
+				<button
+					className="flex aspect-square size-5 items-center justify-center border-r"
+					onClick={() =>
+						unloadProduct({
+							productId: productInCart.product.id,
+							variantSku: productInCart.variant.sku,
+						})
+					}
+					aria-label={matchLang({
+						[Lang.English]: `Decrease quantity of ${productInCart.product.title} (${productInCart.variant.title})`,
+						[Lang.Vietnamese]: `Giảm số lượng ${productInCart.product.title} (${productInCart.variant.title})`,
+					})(locale)}
+				>
+					-
+				</button>
+				<span className="text-sm">{productInCart.quantity}</span>
+				<button
+					className="flex aspect-square size-5 items-center justify-center border-l"
+					onClick={() => loadProduct(productInCart)}
+					aria-label={matchLang({
+						[Lang.English]: `Increase quantity of ${productInCart.product.title} (${productInCart.variant.title})`,
+						[Lang.Vietnamese]: `Tăng số lượng ${productInCart.product.title} (${productInCart.variant.title})`,
+					})(locale)}
+				>
+					+
+				</button>
+			</div>
+			<div
+				style={{ gridArea: 'price' }}
+				className={cn('place-self-end text-lg font-semibold text-primary', phudu.className)}
+			>
+				{formatPrice(productInCart.variant.price ?? 0)}
+			</div>
+		</div>
+	)
+}
+
 export function CartListClient({
 	className,
 	showCheckbox,
@@ -27,120 +141,19 @@ export function CartListClient({
 	className?: string
 	locale?: Lang
 }): React.JSX.Element {
-	const { cart, removeProduct, toggleCheck, loadProduct, unloadProduct } = useCartManager({
+	const { cart } = useCartManager({
 		syncWithLocalStorage: true,
 	})
 
 	return (
 		<div className={cn('flex flex-col gap-5', className)}>
 			{cart.map((item) => (
-				<div
+				<CartItem
 					key={`${item.product.id}-${item.variant.sku}`}
-					className={cn('grid items-center gap-3', {
-						'grid-cols-[auto_auto_1fr_auto]': showCheckbox,
-						'grid-cols-[auto_1fr_auto]': !showCheckbox,
-					})}
-					style={{
-						gridTemplateAreas: `"${showCheckbox ? 'select ' : ''}img title remove"
-																"${showCheckbox ? 'select ' : ''}img quantity price"`,
-					}}
-				>
-					{showCheckbox && (
-						<Checkbox
-							style={{ gridArea: 'select' }}
-							className="place-self-center"
-							aria-checked={item.checked}
-							checked={item.checked}
-							onCheckedChange={(checked) =>
-								toggleCheck({
-									productId: item.product.id,
-									variantSku: item.variant.sku,
-									checked: !!checked,
-								})
-							}
-						/>
-					)}
-					<Link href={`/product/${item.product.slug}`} style={{ gridArea: 'img' }}>
-						<HeadlessImage
-							media={item.variant.image}
-							alt={matchLang({
-								[Lang.English]: `${item.product.title}'s image`,
-								[Lang.Vietnamese]: `Ảnh ${item.product.title}`,
-							})(locale)}
-							placeholder={{ width: 100, height: 100 }}
-							className="size-[3.75rem] rounded-lg object-cover"
-						/>
-					</Link>
-					<Link
-						href={`/product/${item.product.slug}`}
-						style={{ gridArea: 'title' }}
-						className="text-lg font-semibold text-primary"
-					>
-						{item.product.title}
-					</Link>
-					<button
-						style={{ gridArea: 'remove' }}
-						className="place-self-end self-start text-primary"
-						onClick={() =>
-							removeProduct({
-								productId: item.product.id,
-								variantSku: item.variant.sku,
-							})
-						}
-						aria-label={matchLang({
-							[Lang.English]: `Remove ${item.product.title} (${item.variant.title}) from cart`,
-							[Lang.Vietnamese]: `Xoá ${item.product.title} (${item.variant.title}) khỏi giỏ hàng`,
-						})(locale)}
-					>
-						<X />
-					</button>
-					<div
-						style={{ gridArea: 'quantity' }}
-						className="flex h-5 w-[6.6rem] items-center justify-between border"
-					>
-						<button
-							className="flex aspect-square size-5 items-center justify-center border-r disabled:text-muted-foreground"
-							onClick={(e) => {
-								e.preventDefault()
-								unloadProduct({
-									productId: item.product.id,
-									variantSku: item.variant.sku,
-								})
-							}}
-							aria-label={matchLang({
-								[Lang.English]: `Decrease quantity of ${item.product.title} (${item.variant.title})`,
-								[Lang.Vietnamese]: `Giảm số lượng ${item.product.title} (${item.variant.title})`,
-							})(locale)}
-							disabled={item.quantity <= 1}
-						>
-							-
-						</button>
-						<span className="text-sm">{item.quantity}</span>
-						<button
-							className="flex aspect-square size-5 items-center justify-center border-l disabled:text-muted-foreground"
-							onClick={(e) => {
-								e.preventDefault()
-								loadProduct(item)
-							}}
-							aria-label={matchLang({
-								[Lang.English]: `Increase quantity of ${item.product.title} (${item.variant.title})`,
-								[Lang.Vietnamese]: `Tăng số lượng ${item.product.title} (${item.variant.title})`,
-							})(locale)}
-							disabled={item.quantity >= (item.variant.stock ?? 0)}
-						>
-							+
-						</button>
-					</div>
-					<div
-						style={{ gridArea: 'price' }}
-						className={cn(
-							'place-self-end text-lg font-semibold text-primary',
-							phudu.className,
-						)}
-					>
-						{formatPrice(item.variant.price ?? 0)}
-					</div>
-				</div>
+					productInCart={item}
+					showCheckbox={showCheckbox}
+					locale={locale}
+				/>
 			))}
 		</div>
 	)
