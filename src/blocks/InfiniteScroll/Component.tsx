53e3ec166dce34afff1fdf3069ec35b35149a7cb
Delnegend <kiennguyen19323@gmail.com>
2025-06-21 19:13:08 +0700

    refactor(`blocks/InfiniteScroll`): properly handle number type; rename component

--- src/blocks/InfiniteScroll/Component.tsx
+++ src/blocks/InfiniteScroll/Component.tsx
@@ -1,22 +1,40 @@
+import config from '@payload-config'
+import { getPayload } from 'payload'
+
 import { InfiniteScrollBlockProps } from '@/payload-types'
+import { depthHandler } from '@/utilities/depthHandler'
 import { Lang } from '@/utilities/lang'
 
-import { InfiniteScrollBlockCC } from './Component.client'
+import { INTERNAL_InfiniteScrollBlock } from './Component.client'
 
-export function InfiniteScrollBlock({
+export async function InfiniteScrollBlock({
 	graphic,
 	animationDuration,
 	__locale,
 }: InfiniteScrollBlockProps & {
 	__locale?: Lang
-}): React.JSX.Element {
-	if (typeof graphic === 'number') {
-		return <></>
-	}
+}): Promise<React.JSX.Element> {
+	const {
+		data: graphic_,
+		ok,
+		error,
+	} = await depthHandler({
+		data: graphic,
+		fetch: async (id) => {
+			const result = await (
+				await getPayload({ config })
+			).findByID({
+				collection: 'media',
+				id,
+			})
+			return result
+		},
+	})
+	if (!ok) console.error('Error fetching graphic for InfiniteScrollBlock:', error)
 
 	return (
-		<InfiniteScrollBlockCC
-			graphic={graphic}
+		<INTERNAL_InfiniteScrollBlock
+			graphic={graphic_}
 			animationDuration={animationDuration ?? 10}
 			locale={__locale}
 		/>
