b1e626e94eadcf90677949916673651d86a3c533
Delnegend <kiennguyen19323@gmail.com>
2025-06-23 13:17:58 +0700

    feat(`blocks/Certificates,Highlight,HowToUseProduct`): responsive

--- src/blocks/HighlightCenter/Component.tsx
+++ src/blocks/HighlightCenter/Component.tsx
@@ -17,7 +17,7 @@ export function HighlightCenterBlock(
 	},
 ): React.JSX.Element {
 	return (
-		<div className="safe-width my-28 flex flex-col text-primary">
+		<div className="safe-width my-28 flex flex-col text-primary max-md:max-w-md">
 			<div className="flex w-full flex-col items-center justify-center">
 				<HeadlessImage
 					media={props.image}
@@ -26,15 +26,17 @@ export function HighlightCenterBlock(
 						[Lang.Vietnamese]: 'Hình ảnh nổi bật ở giữa',
 					})(props.__locale)}
 					placeholder={{ width: 600, height: 600 }}
-					className="aspect-square w-[32.75rem] overflow-hidden rounded-full object-cover"
+					className="aspect-square size-[9.75rem] overflow-hidden rounded-full object-cover md:size-[32.75rem]"
 				/>
-				<div className={cn('relative mt-28 text-5xl', phudu.className)}>
+				<div className={cn('relative mt-9 text-base md:mt-28 md:text-5xl', phudu.className)}>
 					{props.order.toString().padStart(2, '0')}
-					<div className="absolute left-1/2 top-[-13.5rem] h-[13rem] w-[2px] -translate-x-1/2 bg-primary" />
+					<div className="absolute left-1/2 top-[-4.5rem] h-16 w-[2px] -translate-x-1/2 bg-primary md:top-[-13.5rem] md:h-[13rem]" />
 				</div>
-				<div className="my-4 text-center font-serif text-[2rem] italic">{props.title}</div>
+				<div className="my-1 text-center font-serif text-base italic md:my-4 md:text-[2rem]">
+					{props.title}
+				</div>
 			</div>
-			<p className="text-balance text-center text-[2rem]">{props.description}</p>
+			<p className="text-balance text-center text-base md:text-[2rem]">{props.description}</p>
 		</div>
 	)
 }
