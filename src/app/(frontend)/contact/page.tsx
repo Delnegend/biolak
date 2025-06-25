ebb9fe70a3cb83a87896f6cc281b7a6baf944971
Delnegend <kiennguyen19323@gmail.com>
2025-06-26 02:53:51 +0700

    refactor(app/fe/contact): rm unused page

--- src/app/(frontend)/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ContactFormGlobalComponent } from '@/globals/ContactForm/Component'
-
-export default async function Contact(): Promise<React.JSX.Element> {
-	return (
-		<div className="mx-auto my-28 w-full max-w-[932px]">
-			<ContactFormGlobalComponent />
-		</div>
-	)
-}
