7e9d4bbc134ce8f77c8e92c1086cb58d8c8bc8cb
Delnegend <kiennguyen19323@gmail.com>
2025-06-27 02:56:53 +0700

    fix(middleware): prohibit sort by orders in products

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,12 +8,26 @@ export function middleware(request: NextRequest) {
 			return Response.redirect(new URL('/home', request.url), 307)
 		case '/search':
 			return Response.redirect(new URL('/home', request.url), 307)
-		default:
+		case '/admin/collections/products':
+			const sort = request.nextUrl.searchParams.get('sort')
+			if (!sort || sort === 'orders') {
+				const url = new URL(request.url)
+				url.searchParams.set('sort', 'title')
+				return NextResponse.rewrite(url)
+			}
 			const headers = new Headers(request.headers)
 			headers.set(HeaderName.CurrentPath, request.nextUrl.pathname)
 			return NextResponse.next({
 				request: {
 					headers,
+				},
+			})
+		default:
+			const headersDefault = new Headers(request.headers)
+			headersDefault.set(HeaderName.CurrentPath, request.nextUrl.pathname)
+			return NextResponse.next({
+				request: {
+					headers: headersDefault,
 				},
 			})
 	}
