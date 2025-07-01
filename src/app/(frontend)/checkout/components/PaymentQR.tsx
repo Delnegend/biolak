d56f4ff09724f0673a2f045cd2d563aec6748e48
Delnegend <kiennguyen19323@gmail.com>
2025-07-01 22:25:50 +0700

    refactor(app/fe/checkout/compoennts): replace next's image w/ img

--- src/app/(frontend)/checkout/components/PaymentQR.tsx
+++ src/app/(frontend)/checkout/components/PaymentQR.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-
 import { Lang } from '@/utilities/lang'
 import { matchLang } from '@/utilities/matchLang'
 export function PaymentQR({
@@ -16,7 +14,8 @@ export function PaymentQR({
 	bankAccountNumber: string
 }): React.JSX.Element {
 	return (
-		<Image
+		// eslint-disable-next-line @next/next/no-img-element
+		<img
 			src={`https://qr.sepay.vn/img?acc=${bankAccountNumber}&bank=${bankName}&amount=${amount}&des=blck-${invoiceId}&download=true`}
 			alt={matchLang({
 				[Lang.English]: 'Bank Transfer QR',
