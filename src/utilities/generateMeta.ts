f9e3840e517d2665e8b4d45c521f09f435ef01c3
Delnegend <kiennguyen19323@gmail.com>
2025-08-10 17:00:24 +0700

    refactor(utils/genMeta,mergeOpenGraph): cleaner defaults

--- src/utilities/generateMeta.ts
+++ src/utilities/generateMeta.ts
@@ -1,48 +1,57 @@
+import config from '@payload-config'
 import type { Metadata } from 'next'
+import { getPayload } from 'payload'
 
-import type { Config, Media, Page, Post, Product } from '../payload-types'
-import { getServerSideURL } from './getURL'
-import { mergeOpenGraph } from './mergeOpenGraph'
+import { MediaSlug } from '@/collections/Media/slug'
+import { GeneralGlobalSlug } from '@/globals/General/config'
 
-function getImageURL(image?: Media | Config['db']['defaultIDType'] | null): string {
-	const serverUrl = getServerSideURL()
+import { GeneralGlobal, type Page, type Post, type Product } from '../payload-types'
+import { depthHandler } from './depthHandler'
+import { getCachedGlobal } from './getGlobals'
+import { tryCatch } from './tryCatch'
 
-	let url = 'https://placehold.co/1280x720'
-
-	if (image && typeof image === 'object' && 'url' in image) {
-		const ogUrl = image.sizes?.og?.url
-
-		url = ogUrl ? serverUrl + ogUrl : serverUrl + image.url
-	}
-
-	return url
-}
-
-// TODO: use the first image in the gallery if available for Product
 export async function generateMeta(args: {
 	doc: Partial<Page> | Partial<Post> | Partial<Product> | null
 }): Promise<Metadata> {
-	const { doc } = args
-	const meta = doc?.meta?.meta
+	const meta = args.doc?.meta?.meta
 
-	const ogImage = getImageURL(
-		meta && 'image' in meta && meta.image && typeof meta.image === 'object' ? meta.image : null,
-	)
-	const title = meta && 'title' in meta && meta.title ? `${meta.title} | BioLAK` : 'BioLAK'
-	const description = meta && 'description' in meta && meta.description ? meta.description : ''
+	const {
+		data: payload,
+		ok: payloadOk,
+		error: payloadError,
+	} = await tryCatch(() => getPayload({ config }))
+	if (!payloadOk) throw new Error(`Failed to initialize Payload: ${payloadError}`)
+	const generalGlobal = await getCachedGlobal<GeneralGlobal>(GeneralGlobalSlug)()
+
+	const product = args.doc as Partial<Product>
+	const productImg = !!product.gallery ? product.gallery[0] : null
+
+	const img = await depthHandler({
+		data: meta?.image ?? productImg ?? generalGlobal.siteBanner,
+		fetch: (id) =>
+			payload.findByID({
+				id,
+				collection: MediaSlug,
+				depth: 0,
+			}),
+	})
+	const title = !!meta?.title ? `${meta.title} | BioLAK` : 'BioLAK'
+	const description = meta?.description ?? generalGlobal.description ?? ''
 
 	return {
+		title,
 		description,
-		openGraph: mergeOpenGraph({
+		openGraph: {
+			title,
 			description,
+			type: 'website',
 			images: [
 				{
-					url: ogImage,
+					url: !!img.data?.url ? img.data?.url : 'https://placehold.co/1280x720',
 				},
 			],
-			title,
-			url: Array.isArray(doc?.slug) ? doc?.slug.join('/') : '/',
-		}),
-		title,
+			url: Array.isArray(args.doc?.slug) ? args.doc?.slug.join('/') : '/',
+			siteName: 'BioLAK',
+		},
 	}
 }
--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -1,23 +1,59 @@
+import config from '@payload-config'
 import type { Metadata } from 'next'
+import { getPayload } from 'payload'
 
-import { getServerSideURL } from './getURL'
+import { MediaSlug } from '@/collections/Media/slug'
+import { GeneralGlobalSlug } from '@/globals/General/config'
+import { GeneralGlobal } from '@/payload-types'
 
-const defaultOpenGraph: Metadata['openGraph'] = {
-	type: 'website',
-	description: 'An open-source website built with Payload and Next.js.',
-	images: [
-		{
-			url: `${getServerSideURL()}/website-template-OG.webp`,
-		},
-	],
-	siteName: 'Payload Website Template',
-	title: 'Payload Website Template',
-}
+import { cnsoleBuilder } from './cnsole'
+import { depthHandler } from './depthHandler'
+import { getCachedGlobal } from './getGlobals'
+import { tryCatch } from './tryCatch'
 
-export function mergeOpenGraph(og?: Metadata['openGraph']): Metadata['openGraph'] {
+const cnsole = cnsoleBuilder('mergeOpenGraph')
+
+export async function mergeOpenGraph(og?: Metadata['openGraph']): Promise<Metadata['openGraph']> {
+	const generalGlobal = await getCachedGlobal<GeneralGlobal>(GeneralGlobalSlug)()
+	const {
+		data: payload,
+		ok: payloadOk,
+		error: payloadError,
+	} = await tryCatch(() => getPayload({ config }))
+	if (!payloadOk) cnsole.error(`Failed to initialize Payload: ${payloadError}`)
+
+	const {
+		data: siteBanner,
+		ok: siteBannerOk,
+		error: siteBannerError,
+	} = await depthHandler({
+		data: generalGlobal.siteBanner,
+		fetch: (id) =>
+			payloadOk
+				? payload.findByID({
+						id,
+						collection: MediaSlug,
+						depth: 0,
+					})
+				: null,
+	})
+	if (!siteBannerOk) cnsole.error(`Failed to fetch site banner: ${siteBannerError}`)
+
 	return {
-		...defaultOpenGraph,
+		...{
+			type: 'website',
+			description: 'An open-source website built with Payload and Next.js.',
+			images: [
+				{
+					url: siteBanner?.url ?? 'https://placehold.co/1280x720',
+				},
+			],
+			siteName: 'BioLAK',
+			title: 'BioLAK',
+		},
 		...og,
-		images: og?.images ? og.images : defaultOpenGraph?.images,
+		images: og?.images ?? {
+			url: siteBanner?.url ?? 'https://placehold.co/1280x720',
+		},
 	}
 }
