1ebe10001bb7698df126cf46c6bedba453069da3
Delnegend <kiennguyen19323@gmail.com>
2025-08-11 04:12:36 +0700

    refactor(`app/fe/checkout/actions/cartSchema`): rename fields

--- src/app/(frontend)/checkout/actions/cartSchema.ts
+++ src/app/(frontend)/checkout/actions/cartSchema.ts
@@ -2,8 +2,8 @@ import z from 'zod/v4'
 
 export const cartSchema = z.array(
 	z.object({
-		product: z.number().int(),
-		sku: z.string(),
+		productId: z.number().int(),
+		productSku: z.string(),
 		quantity: z.number().int().min(1),
 	}),
 )
