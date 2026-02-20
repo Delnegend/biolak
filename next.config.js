b3afab574585349876f867fbaf9e135e0b96137c
Delnegend <kiennguyen19323@gmail.com>
2026-02-20 17:47:18 +0700

    refactor(next.config): easier to read logic

--- next.config.js
+++ next.config.js
@@ -1,22 +1,27 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 import redirects from './redirects.js'
 
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-	? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-	: undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+const NEXT_PUBLIC_SERVER_URL = (() => {
+	if (process.env.VERCEL_PROJECT_PRODUCTION_URL)
+		return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+
+	if (process.env.NEXT_PUBLIC_SERVER_URL) return process.env.NEXT_PUBLIC_SERVER_URL
+
+	return 'http://localhost:3000'
+})()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
 		remotePatterns: [
-			...[NEXT_PUBLIC_SERVER_URL].map((item) => {
-				const url = new URL(item)
+			(() => {
+				const url = new URL(NEXT_PUBLIC_SERVER_URL)
 
 				return {
 					hostname: url.hostname,
 					protocol: url.protocol.replace(':', ''),
 				}
-			}),
+			})(),
 			new URL('https://placehold.co/**'),
 		],
 	},
